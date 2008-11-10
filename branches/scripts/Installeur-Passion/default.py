63db651a59b804bac282f04ed0ec8dd884fb32fe
FrostBox360 <FrostBox360@cc7b12b2-8c0a-11dd-abc6-8b02bc135b05>
2008-11-10 19:50:23 +0000

    ajout: De WindowXml pour le settings -> voir module dialog_script_settings.py skin: ajout des images dans le themes par défaut pour windowxml, future renommage des autres en passion-blabla.bla retirer: class SettingsWindow(xbmcgui.WindowDialog): du module INSTALLEUR.py commencer l'implantation du système de langue: voir le dossier language et pour methode d'utilisation voir default.py, dialog_script_settings.py ou INSTALLEUR.py nettoyer un peu le code du default.py

--- branches/scripts/Installeur-Passion/default.py
+++ branches/scripts/Installeur-Passion/default.py
@@ -7,27 +7,29 @@ __url__    = "http://passion-xbmc.org/index.php"
 __svn_url__ = "http://code.google.com/p/passion-xbmc/source/browse/#svn/trunk/Scripts/Installeur-Passion"
 __credits__ = "Team XBMC, http://xbmc.org/"
 __platform__  = "xbmc media center"
-__date__    = "08-11-2008"
+__date__    = "10-11-2008"
 __version__ = "1.0.0dev"
 __svn_revision__ = 0
 
 
 import os
+import sys
 import ConfigParser
+
 import xbmc
 import xbmcgui
-import shutil
-import sys
-#import traceback
-
-if sys.modules.has_key("CONF"):
-    del sys.modules['CONF']
-if sys.modules.has_key("CHECKMAJ"):
-    del sys.modules['CHECKMAJ']
-if sys.modules.has_key("INSTALLEUR"):
-    del sys.modules['INSTALLEUR']
 
 from resources.libs.script_log import *
+
+
+# INITIALISATION CHEMIN RACINE
+ROOTDIR = os.getcwd().replace( ";", "" )
+
+#FONCTION POUR RECUPERER LES LABELS DE LA LANGUE.
+__language__ = xbmc.Language( ROOTDIR, "french" ).getLocalizedString
+
+DIALOG_PROGRESS = xbmcgui.DialogProgress()
+
 
 def MAIN():
     LOG( LOG_INFO, "****************************************************************" )
@@ -35,85 +37,51 @@ def MAIN():
     LOG( LOG_INFO, "****************************************************************" )
 
 
-    ##############################################################################
-    #               Initialisation chemins de fichier locaux                     #
-    ##############################################################################
-    ROOTDIR = os.getcwd().replace(';','')
-
-    dp = xbmcgui.DialogProgress()
-    dp.create("Installateur Passion","","")
+    # INITIALISATION CHEMINS DE FICHIER LOCAUX
+    DIALOG_PROGRESS.create( __language__( 32000 ), "", "" )
 
     fichier = os.path.join(ROOTDIR, "resources", "conf.cfg")
     config = ConfigParser.ConfigParser()
     config.read(fichier)
 
-    pathok = config.getboolean('InstallPath','pathok')
-
-    if pathok == False:
-        ##########################################################################
-        #               Generation des informations locales                      #
-        ##########################################################################
-        dp.update(50,"Configuration du systeme","Veuillez patienter...")
+    if not config.getboolean('InstallPath','pathok'):
+        # GENERATION DES INFORMATIONS LOCALES
+        DIALOG_PROGRESS.update( -1, __language__( 32101 ), __language__( 32110 ) )
         from resources.libs import CONF
         CONF.SetConfiguration()
-        #dp.close()
 
-    ##############################################################################
-    #                   Verification de la mise a jour                           #
-    ##############################################################################
-    dp.update(75, "Verification de la mise a jour","Veuillez patienter...")
+    # VERIFICATION DE LA MISE A JOUR 
+    DIALOG_PROGRESS.update( -1, __language__( 32102 ), __language__( 32110 ) )
     from resources.libs import CHECKMAJ
     CHECKMAJ.go()
 
-    dp.update(100, ""," ")
     config.read(fichier)
-    updating = config.getboolean('Version','UPDATING')
 
-    dp.close()
-
-    if updating == False:
+    if not config.getboolean('Version','UPDATING'):
         try:
-            ##########################################################################
-            #               Lancement du script                                      #
-            ##########################################################################
+            # LANCEMENT DU SCRIPT
             from resources.libs import INSTALLEUR
             INSTALLEUR.go()
-        except Exception, e:
-            #print "default : Exception pendant le chargement et/ou l'utilisation de l'INSTALLEUR",e
+        except:
             #dialogError = xbmcgui.Dialog()
             #dialogError.ok("Erreur", "Exception durant l'initialisation")
-            #print ("error/default: " + str(sys.exc_info()[0]))
             EXC_INFO( LOG_ERROR, sys.exc_info() )
-            #traceback.print_exc()
     else:
-        ##########################################################################
-        #               Lancement de la mise a jour                              #
-        ##########################################################################
+        # LANCEMENT DE LA MISE A JOUR
         try:
-            scriptmaj = updating = config.get('Version','SCRIPTMAJ')
-
-            # Ajout au sys PATH le chemin du script d'install
-            dirLibName = os.path.dirname(scriptmaj) # recuperation du chemin du repertoire script
-            sys.path.append(dirLibName)
-            fileName = os.path.basename(scriptmaj)
-            libName  = fileName.replace(".py","")
-            # Lance le script recupere du server dans un sous processus
-            #xbmc.executebuiltin('XBMC.RunScript(%s)'%scriptmaj)
+            scriptmaj = config.get('Version','SCRIPTMAJ')
             xbmc.executescript(scriptmaj)
-            #import INSTALLEUR
+
+            #from resources.libs import INSTALLEUR
             #INSTALLEUR.go()
-            #mon_module_import = "passion"
-            #exec("import %s"%libName)
-            #exec("%s.go()"%libName)
-        except Exception, e:
-            #print "default : Exception pendant le chargement et/ou La mise a jour",e
+        except:
             LOG( LOG_ERROR, "default : Exception pendant le chargement et/ou La mise a jour" )
             #dialogError = xbmcgui.Dialog()
             #dialogError.ok("Erreur", "Exception durant l'initialisation")
-            #print ("error/default: " + str(sys.exc_info()[0]))
             EXC_INFO( LOG_ERROR, sys.exc_info() )
-            #traceback.print_exc()
-            #print "default : FIN DU SCRIPT"
+
+    DIALOG_PROGRESS.close()
+
 
 if __name__ == "__main__":
     MAIN()
---
+++ branches/scripts/Installeur-Passion/resources/libs/dialog_script_settings.py
@@ -0,0 +1,141 @@
+
+#Modules general
+import os
+import sys
+
+#modules XBMC
+import xbmc
+import xbmcgui
+
+#modules custom
+from script_log import *
+
+
+#REPERTOIRE RACINE ( default.py )
+CWD = os.getcwd().rstrip( ";" )
+
+#FONCTION POUR RECUPERER LES LABELS DE LA LANGUE.
+_ = sys.modules[ "__main__" ].__language__
+
+# script constants
+__script__ = sys.modules[ "__main__" ].__script__
+try: __svn_revision__ = sys.modules[ "__main__" ].__svn_revision__
+except: __svn_revision__ = 0
+if not __svn_revision__: __svn_revision__ = "0"
+__version__ = "%s.%s" % ( sys.modules[ "__main__" ].__version__, __svn_revision__ )
+
+
+#FONCTION POUR RECUPERER LE THEME UTILISE PAR L'UTILISATEUR.
+def getUserSkin():
+    current_skin = xbmc.getSkinDir()
+    force_fallback = os.path.exists( os.path.join( CWD, "resources", "skins", current_skin ) )
+    if not force_fallback: current_skin = "Default"
+    return current_skin, force_fallback
+
+
+class ScriptSettings( xbmcgui.WindowXMLDialog ):
+    def __init__( self, *args, **kwargs ):
+        # __init__ normal de python
+        # On recupere le "self" de le fenetre principal pour benificier de ces variables.
+        self.mainwin = kwargs[ "mainwin" ]
+
+    def onInit( self ):
+        # onInit est pour le windowXML seulement
+        self._get_settings()
+        self._set_controls_labels()
+        self._set_controls_values()
+        self._set_controls_visible()
+
+    def _get_settings( self ):
+        # Get settings
+        try:
+            self.configManager = self.mainwin.configManager
+            self.xbmcXmlUpdate  = self.configManager.getXbmcXmlUpdate()
+        except:
+            EXC_INFO( LOG_ERROR, sys.exc_info(), self )
+            self._close_dialog()
+
+    def _set_controls_labels( self ):
+        # setlabel pour les controles du dialog qui a comme info exemple: id="100" et pour avoir son controle on fait un getControl( 100 )
+        try:
+            self.getControl( 101 ).setLabel( _( 32499 ) % ( __version__, ) )
+            self.getControl( 100 ).setLabel( "%s - %s" % ( _( 32000 ), _( 32500 ), ) )
+            self.getControl( 160 ).setLabel( _( 32501 ) )
+        except:
+            EXC_INFO( LOG_ERROR, sys.exc_info(), self )
+
+    def _set_controls_values( self ):
+        xbmcgui.lock()
+        try:
+            #bouton pour activer desactiver la modification du fichier sources.xml
+            #selon l'etat de self.xbmcXmlUpdate, l'image du radiobutton sera blanc ou non visible
+            self.getControl( 160 ).setSelected( self.xbmcXmlUpdate )
+            #le bouton reset settings est desactiver, le temps d'implanter cette fonction
+            self.getControl( 302 ).setEnabled( 0 )
+            #le bouton credits est desactiver, le temps d'implanter cette fonction
+            self.getControl( 303 ).setEnabled( 0 )
+        except:
+            EXC_INFO( LOG_ERROR, sys.exc_info(), self )
+        xbmcgui.unlock()
+
+    def _set_controls_visible( self ):
+        """
+        ici sa sert a rendre visible les controls qu'on veut voir 
+        pour le moment il y a 1 parametre, donc les autres sont mis non visible
+        pour le futur on pourra les activer au besoin et coder sa fonction
+        penser a retirer les # de bouton_non_visible = [ 170, 180, 190, 200, 210, 220, 230, 240 ] par ordre de grandeur, suivant == 170
+        """
+        xbmcgui.lock()
+        try:
+            bouton_non_visible = [ 170, 180, 190, 200, 210, 220, 230, 240 ]#range( 170, 250, 10 )
+            for control_id in bouton_non_visible:
+                self.getControl( control_id ).setEnabled( False )
+                self.getControl( control_id ).setVisible( False ) 
+        except:
+            EXC_INFO( LOG_ERROR, sys.exc_info(), self )
+        xbmcgui.unlock()
+        
+    def onFocus( self, controlID ):
+        #cette fonction n'est pas utiliser ici, mais dans les XML si besoin
+        pass
+
+    def onClick( self, controlID ):
+        try:
+            if controlID == 160:
+                #bouton pour activer desactiver la modification du fichier sources.xml
+                if not self.xbmcXmlUpdate: self.xbmcXmlUpdate = True #Activer
+                else: self.xbmcXmlUpdate = False #Desactiver
+            elif controlID == 300:
+                #bouton ok on save les changements.
+                self.configManager.setXbmcXmlUpdate( self.xbmcXmlUpdate )
+                self._close_dialog()
+            elif controlID == 301:
+                # bouton annuler on ferme le dialog
+                self._close_dialog()
+            elif controlID == 302:
+                #le bouton reset settings est desactiver, le temps d'implanter cette fonction
+                #print "on reset tous les settings"
+                pass
+            else:
+                pass
+        except:
+            EXC_INFO( LOG_ERROR, sys.exc_info(), self )
+
+    def onAction( self, action ):
+        #( ACTION_PARENT_DIR, ACTION_PREVIOUS_MENU, ACTION_CONTEXT_MENU, )
+        if action in ( 9, 10, 117 ): self._close_dialog()
+
+    def _close_dialog( self ):
+        xbmc.sleep( 100 )
+        self.close()
+
+def show_settings( mainwin ):
+    file_xml = "passion-DialogScriptSettings.xml"
+    #depuis la revision 14811 on a plus besoin de mettre le chemin complet, la racine suffit
+    dir_path = CWD #xbmc.translatePath( os.path.join( CWD, "resources" ) ) 
+    #recupere le nom du skin et si force_fallback est vrai, il va chercher les images du defaultSkin.
+    current_skin, force_fallback = getUserSkin()
+
+    w = ScriptSettings( file_xml, dir_path, current_skin, force_fallback, mainwin=mainwin )
+    w.doModal()
+    del w
