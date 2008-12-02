a6f9e424d36428de21ce2c356efe83c36711ed42
temhil <temhil@cc7b12b2-8c0a-11dd-abc6-8b02bc135b05>
2008-12-02 22:49:45 +0000

    [artePLUS7]: Version Beta1: - conf generee automatiquement lors du 1er demarrage (defaut fr et pas de proxy) - choix de la langue des video via la conf (fr et de)

--- trunk/Plugins/artePLUS7/default.py
+++ trunk/Plugins/artePLUS7/default.py
@@ -10,12 +10,18 @@ Place in Q:\plugins\video\artePLUS7
     - Fixed issue with icons
     - Fixed issue wit video loading using createVideoURL which create a UNRl from the video preview URL
     - Replaced default URL by French one instead of German
-29-11-08 Version Beta1-Dev01 by Temhil
+02-12-08 Version Beta1 by Temhil
     - Added proxy support for people wiht not an PI address in the area supported by Arte website
       Proxy has to be defined in arteplus7.cfg as follow:
       [server]
       proxy_address = http://<IP-adress>
     - Updated getVideoURL in order to use proxy AND load real video URL in wmv container
+    - Added video language support, it has to be defined (fr = fench / de = German) as follow 
+      [user_pref]
+      videos_language = fr
+    - Conf file is generated automatically during 1st startup:
+      -> default video language is French
+      -> default proxy is empty (no proxy)
 """
 import xml.dom.minidom, urllib, os, string, traceback, time, re
 import xbmc, xbmcgui, xbmcplugin
@@ -28,6 +34,108 @@ import ConfigParser
 RootDir  = os.getcwd().replace( ";", "" ) # Create a path with valid format
 cacheDir = os.path.join(RootDir, "cache")
 
+class confManager:
+    """
+    Configuration Manager
+    """
+    def __init__(self,confFilePath):
+        """
+        Load configuration file, check it, and correct it if necessary
+        """
+        self.is_conf_valid   = True
+        self.confFilePath    = confFilePath
+        self.proxy_address   = ""
+        self.videos_language = "fr"
+        try:
+            # Create config parser
+            self.config = ConfigParser.ConfigParser()
+            
+            # Read config from .cfg file
+            # - Open config file
+            self.config.read(confFilePath)
+
+
+            # Check sections exist
+            if (self.config.has_section("server") == False):
+                self.config.add_section("server")
+                self.is_conf_valid = False
+            if (self.config.has_section("user_pref") == False):
+                self.config.add_section("user_pref")
+                self.is_conf_valid = False
+
+            # - Read config from file and correct it if necessary
+            if (self.config.has_option("server", "proxy_address") == False):
+                self.config.set("server", "proxy_address", self.proxy_address)
+                self.is_conf_valid = False
+            else:
+                self.proxy_address = self.config.get("server", "proxy_address")
+                
+            if (self.config.has_option("user_pref", "videos_language") == False):
+                self.config.set("user_pref", "videos_language", self.videos_language)
+                self.is_conf_valid = False
+            else:
+                self.videos_language = self.config.get("user_pref", "videos_language")
+        
+            if (self.is_conf_valid == False):
+                # Update file
+                print "cfg file format wasn't valid: correcting ..."
+                cfgfile=open(self.confFilePath, 'w+')
+                try:
+                    self.config.write(cfgfile)
+                    self.is_conf_valid = True
+                except Exception, e:
+                    print("Exception during cfg file update")
+                    print(str(e))
+                    print (str(sys.exc_info()[0]))
+                    traceback.print_exc()
+                cfgfile.close()
+        
+        except IOError:
+            print "Error while loading conf file arteplus7.cfg"
+            print (str(sys.exc_info()[0]))
+            traceback.print_exc(file=sys.stdout)
+
+    def setProxy(self,proxy_address):
+        self.proxy_address   = proxy_address
+        
+        # Set proxy_address parameter
+        self.config.set("server", "proxy_address", proxy_address)
+    
+        # Update file
+        cfgfile=open(self.confFilePath, 'w+')
+        try:
+            self.config.write(cfgfile)
+        except Exception, e:
+            print("Exception during setProxy")
+            print(str(e))
+            print (str(sys.exc_info()[0]))
+            traceback.print_exc()
+        cfgfile.close()
+        
+    def getProxy(self):
+        return self.proxy_address
+    
+    def setVideosLang(self,videos_language):
+        self.videos_language   = videos_language
+        
+        # Set videos_language parameter
+        self.config.set("user_pref", "videos_language", videos_language)
+    
+        # Update file
+        cfgfile=open(self.confFilePath, 'w+')
+        try:
+            self.config.write(cfgfile)
+        except Exception, e:
+            print("Exception during setVideosLang")
+            print(str(e))
+            print (str(sys.exc_info()[0]))
+            traceback.print_exc()
+        cfgfile.close()
+        
+    def getVideosLang(self):
+        return self.videos_language
+    
+
 class SearchParser:
     """
     Parse XML  from arte - code from seeqpod. seeqpod Borrowed this from GameTrailersRSSReader RSS Parser
@@ -35,10 +143,17 @@ class SearchParser:
     def __init__(self):
         # Document Object Model of the XML document
         self.dom = None
-        self.proxy_address = None
-        self.loadConf()
+        self.cfgMgr = confManager(os.path.join(RootDir,"arteplus7.cfg"))
         self.verifrep(cacheDir)
-        
+        if self.cfgMgr.getVideosLang() == 'de':
+            # German videos
+            video_feed_url = "http://plus7.arte.tv/de/streaming-home/1698112,templateId=renderCarouselXml,CmPage=1697480,CmPart=com.arte-tv.streaming.xml"
+        else:
+            # French videos
+            video_feed_url = "http://plus7.arte.tv/fr/streaming-home/1698112,templateId=renderCarouselXml,CmPage=1697480,CmPart=com.arte-tv.streaming.xml"
+            
+        self.feed(video_feed_url)
+        self.parse()
                                            
     def verifrep(self, folder):
         """
@@ -53,29 +168,6 @@ class SearchParser:
             print("Exception while creating folder " + folder)
             print(str(e))
 
-    def loadConf(self):
-        """
-        Load configuration file, check it, and correct it if necessary
-        """
-        try:
-            # Create config parser
-            self.config = ConfigParser.ConfigParser()
-            
-            # Read config from .cfg file
-            # - Open config file
-            self.config.read(os.path.join(RootDir,"arteplus7.cfg"))
-
-            # Check sections exist
-            if (self.config.has_section("server") == True):
-                # - Read config from file and correct it if necessary
-                if (self.config.has_option("server", "proxy_address") == True):
-                    self.proxy_address = self.config.get("server", "proxy_address")
-                    if self.proxy_address == "":
-                        self.proxy_address = None
-        except IOError:
-            print "Error while loading conf file arteplus7.cfg"
-            print (str(sys.exc_info()[0]))
-            traceback.print_exc(file=sys.stdout)
             
     def delFiles(self,folder):
         """
@@ -107,17 +199,18 @@ class SearchParser:
         opener              = None
         videoUrl            = ""
         videoContainerUrl   = "" 
+        proxy_address       = self.cfgMgr.getProxy()
                 
-        if self.proxy_address is None:
+        if proxy_address == "":
             print "getVideoURL - NO proxy defined"
             # create opener
             opener = urllib2.build_opener()
         else:
             print "getVideoURL - proxy DEFINED"
-            print self.proxy_address
+            print proxy_address
             
             # create the proxy handler
-            proxy_handler = urllib2.ProxyHandler({'http': self.proxy_address})
+            proxy_handler = urllib2.ProxyHandler({'http':proxy_address})
             
             # create opener
             opener = urllib2.build_opener(proxy_handler)
@@ -129,8 +222,8 @@ class SearchParser:
         req=urllib2.Request(videoPage)
         videoDoc=urllib2.urlopen(req).read()
         
-#        # Write in a file
-#        open(xbmc.makeLegalFilename(os.path.join(cacheDir, os.path.basename(videoPage))),"w").write(videoDoc)
+        # Write in a file
+        open(xbmc.makeLegalFilename(os.path.join(cacheDir, os.path.basename(videoPage))),"w").write(videoDoc)
 
         # Extract the URL of the video URL container file (wmv)
         match = re.search(r'availableFormats\[\d]\[\"format\"\] = \"WMV\";\n *?availableFormats\[\d\]\["quality\"] = \"HQ\";\n *?availableFormats\[\d]\[\"url\"] = "(.*?)\?obj', videoDoc)
@@ -142,8 +235,8 @@ class SearchParser:
             videoUrlFile = urllib.urlopen(videoContainerUrl).read()
             #TODO: use urllib2 everywhere, using a mix or urlib and urllib2 is not very elegant
             
-#            # Write in a file
-#            open(xbmc.makeLegalFilename(os.path.join(cacheDir, os.path.basename(videoContainerUrl))),"w").write(videoUrlFile)
+            # Write in a file
+            open(xbmc.makeLegalFilename(os.path.join(cacheDir, os.path.basename(videoContainerUrl))),"w").write(videoUrlFile)
             
             # Extract the URL of the video (usually mms)
             matchVideoURL = re.search(r'<REF HREF=\"(.*?)\" />', videoUrlFile)
@@ -260,8 +353,37 @@ class SearchParser:
         return itemObjects
 
 
+##Il faut parser les paramètres
+#stringparams = sys.argv[2] #les paramètres sont sur le 3ieme argument passé au script
+#try:
+#    if stringparams[0]=="?":#pour enlever le ? si il est en début des paramètres
+#        stringparams=stringparams[1:]
+#except:
+#    pass
+#parametres={}
+#for param in stringparams.split("&"):#on découpe les paramètres sur le '&'
+#    try:
+#        cle,valeur=param.split("=")#on sépare les couples clé/valeur
+#    except:
+#        cle=param
+#        valeur=""
+#    parametres[cle]=valeur #on rempli le dictionnaire des paramètres
+##voilà, 'parametres' contient les paramètres parsés
+#
+#if "loadvideo" in parametres.keys():
+#    #à priori non utilisé !
+#    #on liste les themes
+#    show_themes()
+#else:
+#    # video list
+#    s=SearchParser()
+#    s.feed("http://plus7.arte.tv/fr/streaming-home/1698112,templateId=renderCarouselXml,CmPage=1697480,CmPart=com.arte-tv.streaming.xml")
+#    s.parse()
+#    
+#    xbmcplugin.endOfDirectory(int(sys.argv[1]))
+
 s=SearchParser()
-s.feed("http://plus7.arte.tv/fr/streaming-home/1698112,templateId=renderCarouselXml,CmPage=1697480,CmPart=com.arte-tv.streaming.xml")
-s.parse()
-    
+#s.feed("http://plus7.arte.tv/fr/streaming-home/1698112,templateId=renderCarouselXml,CmPage=1697480,CmPart=com.arte-tv.streaming.xml")
+#s.parse()
+
 xbmcplugin.endOfDirectory(int(sys.argv[1]))
