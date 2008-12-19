d7c0985f11ad5f362eb6e9f1abacb74b6d266927
temhil <temhil@cc7b12b2-8c0a-11dd-abc6-8b02bc135b05>
2008-12-19 20:46:47 +0000

    [Installeur-Passion-ServerSide]: Mise a jour du script d'installation afin de permettre l'install de la nouvcelle structure (winXML) On efface desormais tout le contenu du repertoire (excepté le repertoire cache) avant de dezipper le conten une l'archive du script a jour.

--- trunk/Scripts/Installeur-Passion-ServerSide/INSTALLMAJ2.py
+++ trunk/Scripts/Installeur-Passion-ServerSide/INSTALLMAJ2.py
@@ -7,12 +7,18 @@ import string
 import sys
 import traceback
 
+__language__ = xbmc.getLanguage()
+
 print "****************************************************************"
-print "                      Mise  a jour du script                   "
+print "                Updating Installer-passion script               "
 print "****************************************************************"
+
 
 
 def zipextraction (archive,pathdst):
+    """
+    Decompresse l'archive zip 'archive' au chemin specifié: 'pathdst'
+    """
     idracine = False
     zfile = zipfile.ZipFile(archive, 'r')
     compteurdossier = 0
@@ -38,16 +44,57 @@ def zipextraction (archive,pathdst):
                 try:
                     os.makedirs(filedtst)
                 except Exception, e:
-                    print "Erreur creation dossier de l'archive = ",e
+                    print "Error during directory creation for archive = ",e
 
         else:
             #Extraction des fichiers
             compteurfichier = compteurfichier + 1
-            data = zfile.read(i)                   ## lecture du fichier compresse
-            fp = open(filedtst, "wb")  ## creation en local du nouveau fichier
-            fp.write(data)                         ## ajout des donnees du fichier compresse dans le fichier local
+            data = zfile.read(i)        ## lecture du fichier compresse
+            fp = open(filedtst, "wb")   ## creation en local du nouveau fichier
+            fp.write(data)              ## ajout des donnees du fichier compresse dans le fichier local
             fp.close()
     zfile.close()
+    
+
+def deleteDir(path,keep=[]):
+    """
+    Efface un repertoire et tout son contenu (le repertoire n'a pas besoin d'etre vide)
+    excepté les repertoires (et leur contenu) de la liste keep passée en parametre
+    retourne True si le repertoire est efface False sinon
+    """
+    result = True
+    if os.path.isdir(path):
+        dirItems=os.listdir(path)
+        for item in dirItems:
+            itemFullPath=os.path.join(path, item)
+            try:
+                if os.path.isfile(itemFullPath):
+                    # Fichier
+                    os.remove(itemFullPath)
+                elif os.path.isdir(itemFullPath):
+                    if not(itemFullPath in keep):
+                        # Repertoire
+                        deleteDir(itemFullPath,keep=keep)
+            except:
+                result = False
+                print "deleteDir: Exception during deletion of directory: %s"%path 
+                print ("error/INSTALLMAJ go: " + str(sys.exc_info()[0]))
+                traceback.print_exc()
+
+        # Suppression du repertoire pere
+        if not(path in keep):
+            try:
+                os.rmdir(path)
+            except:
+                result = False
+                print "deleteDir: Exception during deletion of directory: %s"%path 
+                print ("error/INSTALLMAJ go: " + str(sys.exc_info()[0]))
+                traceback.print_exc()
+    else:
+        print "deleteDir: %s is not a directory"%path
+        result = False
+
+    return result
 
 
 def go():
@@ -64,16 +111,30 @@ def go():
         archive     = config.get('Localparam', 'Archive')
         script      = config.get('Localparam', 'Scripttolaunch')       
 
+        dirs2keep = [ passiondir , os.path.dirname(archive) ]
+        
+        # Nettoyage du repertoire du script avant installation de la nouvelle version
+        deleteDir(passiondir,keep=dirs2keep)
+        
+        print "%s content deleted"%passiondir
+
         sys.path.append(passiondir)
     
         dp = xbmcgui.DialogProgress()
-        dp.create("Installeur passion - Mise a jour","Mise a jour du script en cours","Veuillez patienter...")
+        if __language__ == 'french':
+            dp.create("Installeur Passion-XBMC - Mise a jour","Mise a jour du script en cours","Veuillez patienter...")
+        else:
+            dp.create("Installer Passion-XBMC - Update","Script update in progress","Please wait...")
         zipextraction(archive,passiondir)
         dp.close()
         del config #On supprime le config parser
         
         dialogInfo = xbmcgui.Dialog()
-        result = dialogInfo.ok("Installeur passion - Mise a jour", "Mise a jour effectuée", "Vous pouvez desormais relancer le script")
+        if __language__ == 'french':
+            result = dialogInfo.ok("Installeur Passion-XBMC - Mise a jour", "Mise a jour effectuée", "Vous pouvez desormais relancer le script")
+        else:
+            result = dialogInfo.ok("Installer Passion-XBMC - Update", "Update done", "You can now restart the script")
+            
         
     except Exception, e:
         print "INSTALLMAJ : go(): Exception",e
@@ -82,9 +143,11 @@ def go():
         print ("error/INSTALLMAJ go: " + str(sys.exc_info()[0]))
         traceback.print_exc()
 
+
+
 if __name__ == "__main__":
     #ici on pourrait faire des action si le script était lancé en tant que programme
-    print "demarrage du script en tant que programme"
+    print "Start script as a program"
     go()
 else:
     #ici on est en mode librairie importée depuis un programme
