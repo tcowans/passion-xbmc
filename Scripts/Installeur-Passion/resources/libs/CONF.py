809e2b24ec5b0d7dda4016a393a41487d934ec65
temhil <temhil@cc7b12b2-8c0a-11dd-abc6-8b02bc135b05>
2008-12-19 21:22:30 +0000

    [Installeur-Passion]: Recuperation des derniers mises a jour de Frost (r186 danas la branche script)

--- Scripts/Installeur-Passion/resources/libs/CONF.py
+++ Scripts/Installeur-Passion/resources/libs/CONF.py
@@ -1,10 +1,15 @@
 
+#Modules general
 import os
 import sys
 import ConfigParser
 
-#import xbmc
+#modules XBMC
+import xbmc
 from xbmcgui import Dialog
+
+#modules custom
+#from utilities import SYSTEM_PLATFORM, XBMC_ROOT
 
 #module logger
 try:
@@ -16,111 +21,63 @@ except:
 DIALOG_BROWSE = Dialog().browse
 
 
-def SetConfiguration ():
-    """
-    Definit les repertoires locaux de l'utilisateur
-    """
-
+def SetConfiguration():
+    """ Definit les repertoires locaux de l'utilisateur """
+    from utilities import SYSTEM_PLATFORM, XBMC_ROOT
+    
     logger.LOG( logger.LOG_DEBUG, str( "*" * 85 ) )
     logger.LOG( logger.LOG_DEBUG, "Setting Configuration".center( 85 ) )
     logger.LOG( logger.LOG_DEBUG, str( "*" * 85 ) )
+    logger.LOG( logger.LOG_DEBUG, "%s case", SYSTEM_PLATFORM )
 
-    ROOTDIR = os.getcwd().replace(';','')
-    fichier = os.path.join(ROOTDIR, "resources", "conf.cfg")
+    ROOTDIR = os.getcwd().replace( ";", "" )
+    fichier = os.path.join( ROOTDIR, "resources", "conf.cfg" )
     config = ConfigParser.ConfigParser()
-    config.read(fichier)
+    config.read( fichier )
     USRPath = False
 
-    if os.name=='posix':
+    if not os.path.isdir( XBMC_ROOT ):
+        XBMC_ROOT = DIALOG_BROWSE( 0, sys.modules[ "__main__" ].__language__( 100 ), "files" )
+        logger.LOG( logger.LOG_DEBUG, "Other case, XBMC = %s", XBMC_ROOT )
+    config.set( "InstallPath", "path", XBMC_ROOT )
 
-        #Linux Case
-        logger.LOG( logger.LOG_DEBUG, "linux case" )
-
-        if os.path.isdir(".xbmc") == True:
-
-            # Linux normal case
-            XBMC = ".xbmc"+os.sep
-            config.set("InstallPath", "path", XBMC)
-            USRXBMC = os.sep+'usr'+os.sep+'share'+os.sep+'xbmc'+os.sep
-            #Set Linux normal ScraperDir
-            ScraperDir  = os.path.join(USRXBMC, "system"+os.sep+"scrapers"+os.sep+"video")
-            config.set("InstallPath", "ScraperDir", ScraperDir)
-            #Set Linux PMIIIDir
-            PMIIIDir = os.path.join(USRXBMC, "skin")
-            config.set("InstallPath", "PMIIIDir",PMIIIDir)
-            USRPath = True
-
-        else:
-
-            #Linux other case
-            XBMC = DIALOG_BROWSE(0, "Choisissez le dossier d'installation d'XBMC","files")
-            logger.LOG( logger.LOG_DEBUG, "linux other case, XBMC = %s", XBMC )
-            config.set("InstallPath", "path", XBMC)
-            #Set Linux other case ScraperDir
-            ScraperDir  = os.path.join(XBMC, "system"+os.sep+"scrapers"+os.sep+"video")
-            config.set("InstallPath", "ScraperDir", ScraperDir)
-
-
+    if SYSTEM_PLATFORM == "linux":
+        #Set Linux normal ScraperDir
+        ScraperDir = os.path.join( os.sep+"usr", "share", "xbmc", "system", "scrapers", "video" )
+        config.set( "InstallPath", "ScraperDir", ScraperDir )
+        #Set Linux PMIIIDir
+        PMIIIDir = os.path.join( os.sep+"usr", "share", "xbmc", "skin" )
+        config.set( "InstallPath", "PMIIIDir", PMIIIDir )
+        USRPath = True
     else:
-
-        # Xbox and Windows case
-        logger.LOG( logger.LOG_DEBUG, "Xbox and Windows case" )
-
-        if os.path.isdir("Q:"+os.sep) == True:
-
-            # Xbox and Windows normal case
-            XBMC = "Q:"+os.sep
-            config.set("InstallPath", "path", XBMC)
-
-        else:
-
-            # Xbox and Windows other case
-            XBMC = DIALOG_BROWSE(0, "Choisissez le dossier d'installation d'XBMC","files")
-            logger.LOG( logger.LOG_DEBUG, "win other case, XBMC = %s", XBMC )
-            config.set("InstallPath", "path", XBMC)
-
         #Set Win ScraperDir
-        scraperDir  = os.path.join(XBMC, "system"+os.sep+"scrapers"+os.sep+"video")
-        config.set("InstallPath", "ScraperDir", scraperDir)
+        scraperDir = os.path.join( XBMC_ROOT, "system", "scrapers", "video" )
+        config.set( "InstallPath", "ScraperDir", scraperDir )
 
     #Set ScraperType
-    config.set("InstallPath", "USRPath", USRPath)
+    config.set( "InstallPath", "USRPath", USRPath )
 
     #Set ThemesDir
-    ThemesDir   = os.path.join(XBMC, "skin")
-    config.set("InstallPath", "ThemesDir", ThemesDir)
+    config.set( "InstallPath", "ThemesDir", os.path.join( XBMC_ROOT, "skin" ) )
 
     #Set ScriptsDir
-    ScriptsDir   = os.path.join(XBMC, "scripts")
-    config.set("InstallPath", "ScriptsDir", ScriptsDir)
+    config.set( "InstallPath", "ScriptsDir", os.path.join( XBMC_ROOT, "scripts" ) )
 
     #Set PluginDir
-    PluginDir   = os.path.join(XBMC, "plugins")
-    config.set("InstallPath", "PluginDir", PluginDir)
-    PluginMusDir   = os.path.join(XBMC, "plugins" + os.sep + "music")
-    config.set("InstallPath", "PluginMusDir", PluginMusDir)
-    PluginPictDir   = os.path.join(XBMC, "plugins" + os.sep + "pictures")
-    config.set("InstallPath", "PluginPictDir", PluginPictDir)
-    PluginProgDir   = os.path.join(XBMC, "plugins" + os.sep + "programs")
-    config.set("InstallPath", "PluginProgDir", PluginProgDir)
-    PluginVidDir   = os.path.join(XBMC, "plugins" + os.sep + "video")
-    config.set("InstallPath", "PluginVidDir", PluginVidDir)
-    
-    #Set ImageDir
-    #ImageDir = os.path.join(ROOTDIR, "images")
-    ImageDir = os.path.join(ROOTDIR, "resources", "skins", "Default", "media")
-    config.set("InstallPath", "ImageDir", ImageDir)
+    PluginDir = os.path.join( XBMC_ROOT, "plugins" )
+    config.set( "InstallPath", "PluginDir", PluginDir )
+    config.set( "InstallPath", "PluginMusDir", os.path.join( PluginDir, "music" ) )
+    config.set( "InstallPath", "PluginPictDir", os.path.join( PluginDir, "pictures" ) )
+    config.set( "InstallPath", "PluginProgDir", os.path.join( PluginDir, "programs" ) )
+    config.set( "InstallPath", "PluginVidDir", os.path.join( PluginDir, "video" ) )
 
     #Set CacheDir
-    CacheDir = os.path.join(ROOTDIR, "cache")
-    config.set("InstallPath", "CacheDir", CacheDir)
-    
+    config.set( "InstallPath", "CacheDir", os.path.join( ROOTDIR, "cache" ) )
+
     #Set UserDataDir
-    #UserDataDir = xbmc.translatePath( "Q:\\userdata" )
-    UserDataDir = os.path.join(XBMC, "userdata")
-    config.set("InstallPath", "UserDataDir", UserDataDir)
+    config.set( "InstallPath", "UserDataDir", os.path.join( XBMC_ROOT, "userdata" ) )
 
     #Save configuration
-    config.set("InstallPath", "pathok", True)
-    config.write(open(fichier,'w'))
+    config.set( "InstallPath", "pathok", True )
+    config.write( open( fichier, "w" ) )
 
