26dcdf6ea63bb89cbaa4d91e20d19befb482a0b9
seb012007 <seb012007@cc7b12b2-8c0a-11dd-abc6-8b02bc135b05>
2008-12-20 01:24:31 +0000

    Dev Intégration SQL, petit script à part pour faciliter l'intégration des modifications dûes à l'intégration de sqlite3

---
+++ branches/scripts/Dev Intégration SQL/importcsv.php
@@ -0,0 +1,51 @@
+<?php
+
+//Get the database and board information
+require_once(dirname(__FILE__) . '/Settings.php');
+global $db_server, $db_name, $db_user, $db_prefix, $db_passwd;
+
+
+//nom de la table
+$table_name="smf_tp_dlmanager";
+
+//connection à la base de donnée 
+ 
+$link = mysql_connect($db_server, $db_user, $db_passwd) or die("Connexion impossible.");
+$db = mysql_select_db($db_name, $link);
+  
+      
+$resQuery = mysql_query("select `id`, `name`, `description`, `icon`, `category`, `type`, `downloads`, `file`, `created`, `parent`, `screenshot` from " . $table_name); 
+
+header("Content-Type: application/csv-tab-delimited-table"); 
+header("Content-disposition: filename=table.csv"); 
+
+if (mysql_num_rows($resQuery) != 0) { 
+    // titre des colonnes 
+    //$fields = mysql_num_fields($resQuery); 
+    //$i = 0; 
+    //while ($i < $fields) { 
+    //    echo mysql_field_name($resQuery, $i)."|"; 
+    //    $i++; 
+    //} 
+    //echo "\n"; 
+
+    // données de la table 
+    while ($arrSelect = mysql_fetch_array($resQuery, MYSQL_ASSOC)) { 
+        foreach($arrSelect as $elem) {
+            $contenu=utf8_encode($elem);
+            if ($contenu == '')
+            {
+                echo "None|";
+            }
+            else
+            {
+                $textsansbalise = strip_tags($contenu);
+                $text = str_replace("\r\n","</br>",$textsansbalise);
+                //$text = nl2br($contenu);
+                echo "$text|"; 
+            }
+        } 
+        echo "\n"; 
+    } 
+} 
+?> 
