104fccdf73fcd2f3d02789469b61c07b7072ff91
seb012007 <seb012007@cc7b12b2-8c0a-11dd-abc6-8b02bc135b05>
2008-12-26 14:26:02 +0000

    [Installer SQLite] Initialisation des travaux d'intégration au script principal

---
+++ branches/scripts/Installer Passion-XBMC-SQLite/default.py
@@ -0,0 +1,104 @@
+# Installer Passion-XBMC script revision: 216 - built with build.bat version 1.0 #
+
+
+# script constants
+__script__       = "Installer Passion-XBMC"
+__plugin__       = "Unknown"
+__author__       = "Team Passion-XBMC"
+__url__          = "http://passion-xbmc.org/index.php"
+__svn_url__      = "http://code.google.com/p/passion-xbmc/source/browse/#svn/trunk/Scripts/Installer%20Passion-XBMC"
+__credits__      = "Team XBMC, http://xbmc.org/"
+__platform__     = "xbmc media center"
+__date__         = "24-12-2008"
+__version__      = "Beta-Christmas-08"
+__svn_revision__ = "216"
+
+
+#Modules general
+import os
+import sys
+from ConfigParser import ConfigParser
+
+#modules XBMC
+import xbmc
+import xbmcgui
+
+#modules custom
+import resources.libs.script_log as logger
+
+
+# INITIALISATION CHEMIN RACINE
+ROOTDIR = os.getcwd().replace( ";", "" )
+
+#FONCTION POUR RECUPERER LES LABELS DE LA LANGUE. ( ex: __language__( 0 ) = id="0" du fichier strings.xml )
+__language__ = xbmc.Language( ROOTDIR, "french" ).getLocalizedString
+
+DIALOG_PROGRESS = xbmcgui.DialogProgress()
+
+
+def MAIN():
+    logger.LOG( logger.LOG_DEBUG, str( "*" * 85 ) )
+    logger.LOG( logger.LOG_DEBUG, "Lanceur".center( 85 ) )
+    logger.LOG( logger.LOG_DEBUG, str( "*" * 85 ) )
+
+    try:
+        # INITIALISATION CHEMINS DE FICHIER LOCAUX
+        fichier = os.path.join( ROOTDIR, "resources", "conf.cfg" )
+        config = ConfigParser()
+        config.read( fichier )
+
+        DIALOG_PROGRESS.update( -1, __language__( 101 ), __language__( 110 ) )
+        if not config.getboolean( 'InstallPath', 'pathok' ):
+            # GENERATION DES INFORMATIONS LOCALES
+            from resources.libs import CONF
+            CONF.SetConfiguration()
+
+        # VERIFICATION DE LA MISE A JOUR
+        from resources.libs import CHECKMAJ
+        try:
+            from resources.libs.utilities import Settings
+            CHECKMAJ.UPDATE_STARTUP = Settings().get_settings().get( "update_startup", True )
+            del Settings
+        except:
+            logger.EXC_INFO( logger.LOG_ERROR, sys.exc_info() )
+        if CHECKMAJ.UPDATE_STARTUP:
+            DIALOG_PROGRESS.update( -1, __language__( 102 ), __language__( 110 ) )
+        CHECKMAJ.go()
+
+        config.read( fichier )
+
+        dialog_error = False
+        if not config.getboolean( 'Version', 'UPDATING' ):
+            try:
+                # LANCEMENT DU SCRIPT
+                from resources.libs import INSTALLEUR
+                INSTALLEUR.go()
+            except:
+                logger.EXC_INFO( logger.LOG_ERROR, sys.exc_info() )
+                dialog_error = True
+        else:
+            # LANCEMENT DE LA MISE A JOUR
+            try:
+                scriptmaj = config.get( 'Version', 'SCRIPTMAJ' )
+                xbmc.executescript( scriptmaj )
+
+                #from resources.libs import INSTALLEUR
+                #INSTALLEUR.go()
+            except:
+                logger.LOG( logger.LOG_DEBUG, "default : Exception pendant le chargement et/ou La mise a jour" )
+                logger.EXC_INFO( logger.LOG_ERROR, sys.exc_info() )
+                dialog_error = True
+
+        if dialog_error: xbmcgui.Dialog().ok( __language__( 111 ), __language__( 112 ) )
+    except:
+        logger.EXC_INFO( logger.LOG_ERROR, sys.exc_info() )
+    DIALOG_PROGRESS.close()
+
+
+if __name__ == "__main__":
+    try:
+        DIALOG_PROGRESS.create( __language__( 0 ), "", "" )
+        MAIN()
+    except:
+        logger.EXC_INFO( logger.LOG_ERROR, sys.exc_info() )
+        DIALOG_PROGRESS.close()
---
+++ branches/scripts/Installer Passion-XBMC-SQLite/resources/libs/browse_scripts_plugins.py
@@ -0,0 +1,225 @@
+# -*- coding: utf-8 -*-
+import os
+import csv
+import urllib
+import xbmcgui
+import sys
+
+
+#cette methode provient d'apple movie trailer
+# append the proper platforms folder to our path, xbox is the same as win32
+env = ( os.environ.get( "OS", "win32" ), "win32", )[ os.environ.get( "OS", "win32" ) == "xbox" ]
+sys.path.append( os.path.join( os.getcwd(),"resources", "libs", "platform_libraries", env ) )
+from pysqlite2 import dbapi2 as sqlite
+#from platform_libraries.Linux.pysqlite2 import dbapi2 as sqlite
+
+RootDir  = os.getcwd().replace( ";", "" ) # Create a path with valid format
+#cacheDir = os.path.join(RootDir, "cache")
+DB = os.path.join(RootDir, 'DB.db')
+
+
+"""
+STRUCTURE DE LA TABLE :
+(id, name, description, icon, category, type, downloads, file, created, parent, screenshot)
+"""
+
+def maketable():
+    """
+    Fonction de création de la table, à n'appeler que si la table n'existe pas déjà.
+    Pour se faire effectuer un test : if not os.path.isfile(DB):
+    """
+    #On récupère le fichier table.csv
+    result = os.path.join(RootDir, 'table.csv')
+    requete = 'http://passion-xbmc.org/importcsv.php'
+    loc = urllib.URLopener()
+    loc.retrieve(requete, result)
+    loc = urllib.URLopener()
+    loc.retrieve(requete, result)
+    
+    #Initialisation de la base de donnée
+    conn = sqlite.connect(DB)
+    c = conn.cursor()
+    
+    #Creation de la table
+    c.execute('''CREATE TABLE Scripts_Plugins
+    (id int, name text, description text, icon text, category int, type int, downloads int, file text, created unix_timestamp, parent int, screenshot text )''')
+
+    #On sauvegarde les modifications
+    conn.commit()
+
+    #Lecture du fichier table.csv
+    reader = csv.reader(open(result),delimiter = '|')
+
+    for row in reader:
+
+        #on retranche l'occurs de fin de ligne     
+        colonnes = row[:len(row)-1]
+
+        try:
+            #Chaque ligne trouvée dans le table.csv est insérée dans la table
+            c.execute("""INSERT into Scripts_Plugins
+                         (id, name, description, icon, category, type, downloads, file, created, parent, screenshot)
+                         VALUES (?,?,?,?,?,?,?,?,?,?,?)""",colonnes)
+        except Exception, e:
+            print e
+
+        #Sauvegarde des modifications
+        conn.commit()    
+    # On ferme l'instance du curseur
+    c.close()
+
+
+def incat(iditem):
+    """
+    Liste des sous-catégories et fichiers de la catégorie sélectionnée
+    Renvoie un dictionnaire constitué comme suit : {id, name, parent, file,type}
+    Chaque index du dictionnaire renvoie à une liste d'occurences.
+    Il faut l'appeler le numéro id de la catégorie dont on veut obtenir le contenu
+    Si on veut obtenir la liste des catégories racines, il alimenter la fonction avec 0 
+    """
+    
+    #Connection à la base de donnée
+    conn = sqlite.connect(DB)
+    c = conn.cursor()
+    
+    #initialisation des listes du dictionnaire
+    id = []
+    name = []
+    parent = []
+    file = []
+    type =[]
+    
+    #initialisation du dictionnaire
+    dico = {}
+    dico['id'] = id
+    dico['name'] = name
+    dico['parent'] = parent
+    dico['file'] = file
+    dico['type'] = type
+    
+    #requete
+    c.execute('''SELECT id, name, parent, file,type
+                 FROM Scripts_Plugins
+                 WHERE parent = ?
+                 AND type = 'dlcat'  
+                                
+                 UNION ALL      
+                            
+                 SELECT id, name, category, file,type
+                 FROM Scripts_Plugins
+                 WHERE category = ?
+                 AND type = 'dlitem'                 
+                 ORDER BY name ASC
+                ''',(iditem,iditem))
+
+                 
+    #pour chaque colonne fetchée par la requête on alimente une liste, l'ensemble des  
+    #listes constitue le dictionnaire
+    for row in c:
+        print row
+        id.append(row[0])
+        name.append(row[1])
+        parent.append(row[2])
+        file.append(row[3])
+        type.append(row[4])
+  
+    c.close()
+         
+    return dico 
+
+
+def outcat(idparent):
+    """
+    Remonte l'arborescence de la liste. Il faut alimenter cette fonction avec le parent 
+    de la liste qu'on affiche.
+    Renvoie un dictionnaire constitué comme suit : {id, name, parent, file,type}
+    Chaque index du dictionnaire renvoie à une liste d'occurences.
+    """
+    
+    #Connection à la base de donnée
+    conn = sqlite.connect(DB)
+    c = conn.cursor()
+    
+    #initialisation des listes du dictionnaire
+    id = []
+    name = []
+    parent = []
+    file = []
+    type = []
+    
+    #initialisation du dictionnaire
+    dico = {}
+    dico['id'] = id
+    dico['name'] = name
+    dico['parent'] = parent
+    dico['file'] = file
+    dico['type'] = type
+    
+    #requete
+    c.execute('''SELECT id, name, parent, file,type
+                 FROM Scripts_Plugins
+                 WHERE parent = (SELECT parent 
+                                 FROM Scripts_Plugins
+                                 WHERE id = ?)
+                 AND type = 'dlcat'
+                 
+                 UNION ALL
+                 
+                 SELECT id, name, category, file,type
+                 FROM Scripts_Plugins
+                 WHERE category = (SELECT parent 
+                                   FROM Scripts_Plugins
+                                   WHERE id = ?)
+                 AND type = 'dlitem'
+                 
+                 ORDER BY name ASC
+                ''',(idparent,idparent))
+                 
+    #pour chaque colonne fetchée par la requête on alimente une liste, l'ensemble des  
+    #listes constitue le dictionnaire
+    for row in c:
+        id.append(row[0])
+        name.append(row[1])
+        parent.append(row[2])
+        file.append(row[3])
+        type.append(row[4])
+    c.close()
+    
+    return dico
+
+def info(id):
+    """
+    Sert à obtenir des informations sur un fichier.
+    Renvoie un dictionnaire constitué comme suit : 
+    {name, description, icon, downloads, file, created, screenshot}
+    Chaque index du dictionnaire renvoie à une liste d'occurences.
+    Alimenter cette fonction avec l'id du fichier dont on veut obtenir les infos.
+    """
+
+    #Connection à la base de donnée
+    conn = sqlite.connect(DB)
+    c = conn.cursor()  
+    print id
+    
+    c.execute('''SELECT name, description, icon, downloads, file, created, screenshot 
+                 FROM Scripts_Plugins 
+                 WHERE id = ? 
+                 AND type = 'dlitem' 
+                 ''',(id,))
+                 
+    #ici une seule ligne est retournée par la requête
+    #pour chaque colonne fetchée par la requête on alimente un index du dictionnaire
+    dico = {}
+    for row in c:
+        dico['name'] = row[0]
+        dico['description'] = row[1]
+        dico['icon'] = row[2]
+        dico['downloads'] = row[3]
+        dico['file'] = row[4]
+        dico['created'] = row[5]
+        dico['screenshot'] = row[6]
+    c.close()   
+    return dico
+
+
+
