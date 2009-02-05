5bd9995043a9c46d50d38286a666cf684c2b6caa
temhil <temhil@cc7b12b2-8c0a-11dd-abc6-8b02bc135b05>
2009-02-05 01:12:23 +0000

    [Installer Passion-XBMC]: TRUNK - Created new class for parsing XML with ElementTree (this has to be exported in a lib)

--- scripts/Installer Passion-XBMC/resources/libs/dialog_item_descript.py
+++ scripts/Installer Passion-XBMC/resources/libs/dialog_item_descript.py
@@ -8,6 +8,7 @@ import ftplib
 #Other module
 from BeautifulSoup import BeautifulStoneSoup, Tag, NavigableString  #librairie de traitement XML
 from threading import Thread
+import elementtree.ElementTree as ET
 
 #modules XBMC
 import xbmc
@@ -48,6 +49,7 @@ def set_cache_thumb_name( path ):
             os.makedirs( os.path.dirname( thumbnail ) )
         return thumbnail, preview_pic
     except:
+        #import traceback; traceback.print_exc()
         logger.EXC_INFO( logger.LOG_ERROR, sys.exc_info() )
         return "", ""
 
@@ -64,8 +66,8 @@ class InfoWarehouse:
         retourne fileName, title, version, language, date , previewPicture, previewVideoURL, description_fr, description_en
         Fonction abstraite
         """
-        fileName        = itemName # Defaukt value if we don't find itemname
-        title           = itemName # Defaukt value if we don't find itemname
+        fileName        = itemName # Default value if we don't find itemname
+        title           = itemName # Default value if we don't find itemname
         version         = None
         language        = None
         date            = None
@@ -78,14 +80,15 @@ class InfoWarehouse:
         return fileName, title, version, language, date , previewPicture, previewVideoURL, description_fr, description_en, thumbnail
 
 
-class InfoWarehouseXMLFTP( InfoWarehouse ):
+class InfoWarehouseBSoupXMLFTP( InfoWarehouse ):
     """
     Class contenant tous les informations dans un fichier XML sur serveur FTP necessaires a la description d'un item
     Pour des raisons de performance, on instanciera cette classe une seule fois au demarrage du script
+    Cette classe utilise BeautifulSoup
     """
     def __init__( self, *args, **kwargs ):
 
-        logger.LOG( logger.LOG_DEBUG,'InfoWarehouseXMLFTP starts')
+        logger.LOG( logger.LOG_DEBUG,'InfoWarehouseBSoupXMLFTP starts')
         self.mainwin  = kwargs[ "mainwin" ]
 
         self._get_settings()
@@ -130,6 +133,7 @@ class InfoWarehouseXMLFTP( InfoWarehouse ):
                 BASE_THUMBS_PATH = os.path.join( xbmc.translatePath( "P:\\script_data" ), __script__, "Thumbnails" )
                 shutil.rmtree( BASE_THUMBS_PATH )
             except:
+                #import traceback; traceback.print_exc()
                 logger.EXC_INFO( logger.LOG_ERROR, sys.exc_info(), self )
 
     def getInfo( self, itemName=None, itemType=None, itemId=None, updateImage_cb=None, listitem=None ):
@@ -219,6 +223,7 @@ class InfoWarehouseXMLFTP( InfoWarehouse ):
                                 break
                
         except Exception, e:
+            #import traceback; traceback.print_exc()
             logger.EXC_INFO( logger.LOG_ERROR, sys.exc_info(), self )
         
         return fileName, title, version, language, date , previewPicture, previewVideoURL, description_fr, description_en, thumbnail
@@ -254,6 +259,7 @@ class InfoWarehouseXMLFTP( InfoWarehouse ):
                     ftp.retrbinary( 'RETR ' + filetodlUrl, localFile.write )
                 except:
                     logger.EXC_INFO( logger.LOG_ERROR, sys.exc_info(), self )
+                    #import traceback; traceback.print_exc()
                 localFile.close()
                 ftp.quit()
 
@@ -271,6 +277,209 @@ class InfoWarehouseXMLFTP( InfoWarehouse ):
         except:
             logger.LOG( logger.LOG_DEBUG, "_downloaddossier: Exception - Impossible de telecharger le fichier: %s", remoteFilePath )
             logger.EXC_INFO( logger.LOG_ERROR, sys.exc_info(), self )
+            #import traceback; traceback.print_exc()
+
+
+class InfoWarehouseEltTreeXMLFTP( InfoWarehouse ):
+    """
+    Class contenant tous les informations dans un fichier XML sur serveur FTP necessaires a la description d'un item
+    Pour des raisons de performance, on instanciera cette classe une seule fois au demarrage du script
+    Cette classe utilise ElementTree
+    """
+    def __init__( self, *args, **kwargs ):        
+        logger.LOG( logger.LOG_DEBUG,'InfoWarehouseEltTreeXMLFTP starts')
+        self.mainwin  = kwargs[ "mainwin" ]
+
+        self._get_settings()
+        self.thumb_size_on_load = self.mainwin.settings[ "thumb_size" ]
+        
+        # On recupere le fichier de description des items
+        self._downloadFile( self.srvItemDescripDir + self.srvItemDescripFile, isTBN=False )
+        
+        tree = ET.parse( open( os.path.join(self.mainwin.CacheDir,self.srvItemDescripFile ), 'r' ) )
+        self.elemroot = tree.getroot()
+
+    
+    def _getImage( self, pictureURL, updateImage_cb=None, listitem=None ):
+        cached_thumbs = set_cache_thumb_name( pictureURL )
+        if not os.path.exists( cached_thumbs[ 0 ] ) or not os.path.exists( cached_thumbs[ 1 ] ):
+            try: self.getImage_thread.cancel()
+            except: pass
+            self.getImage_thread = Thread( target=self._thread_getImage, args=( pictureURL, updateImage_cb, listitem ) )
+            self.getImage_thread.start()
+
+    def _thread_getImage( self, pictureURL, updateImage_cb=None, listitem=None ):
+        """
+        Recupere l'image dans un thread separe
+        """
+        # Telechargement de l'image
+        self._downloadFile( pictureURL, listitem=listitem )
+        #checkPathPic = set_cache_thumb_name( pictureURL )#os.path.join(self.mainwin.CacheDir, os.path.basename(pictureURL))
+        
+        cached_thumbs = set_cache_thumb_name( pictureURL )
+
+        if os.path.exists( cached_thumbs[ 1 ] ):
+            previewPicture = cached_thumbs[ 1 ]
+        else:
+            previewPicture = None
+        
+        # Notifie la callback de mettre a jour l'image
+        if updateImage_cb:
+            try: updateImage_cb( cached_thumbs[ 1 ] )
+            except TypeError: pass
+
+    def check_thumb_size( self ):
+        if self.thumb_size_on_load != self.mainwin.settings[ "thumb_size" ]:
+            self.thumb_size_on_load = self.mainwin.settings[ "thumb_size" ]
+            try:
+                BASE_THUMBS_PATH = os.path.join( xbmc.translatePath( "P:\\script_data" ), __script__, "Thumbnails" )
+                shutil.rmtree( BASE_THUMBS_PATH )
+            except:
+                logger.EXC_INFO( logger.LOG_ERROR, sys.exc_info(), self )
+
+    def getInfo( self, itemName=None, itemType=None, itemId=None, updateImage_cb=None, listitem=None ):
+        self.check_thumb_size()
+        """ 
+        Lit les info 
+        retourne fileName, title, version, language, date , previewPicture, previewVideoURL, description_fr, description_en
+        Fonction abstraite
+        """
+        """ reads info """
+        fileName        = itemName # Defaukt value if we don't find itemname
+        title           = itemName # Defaukt value if we don't find itemname
+        version         = None
+        language        = None
+        date            = None
+        previewPicture  = None
+        thumbnail       = ""
+        #thumbnail       = None
+        previewVideoURL = None
+        description_fr  = None
+        description_en  = None
+
+        cat = None
+        try:
+            if itemType == "Themes":
+                cat = self.elemroot.find('skins')
+            elif itemType == "Scrapers":
+                cat = self.elemroot.find('scrapers')
+            elif itemType == "Scripts":
+                cat = self.elemroot.find('scripts')
+            elif itemType == "Plugins Musique":
+                cat = self.elemroot.find('plugins').find('musicplugin')
+            elif itemType == "Plugins Images":
+                cat = self.elemroot.find('plugins').find('pictureplugin')
+            elif itemType == "Plugins Programmes":
+                cat = self.elemroot.find('plugins').find('programplugin')
+            elif itemType == "Plugins Vidéos":
+                cat = self.elemroot.find('plugins').find('videoplugin')
+                
+            if cat != None:
+                for item in cat.findall( "entry" ):
+                    filename_raw = item.findtext('fileName')
+                    if ( filename_raw not in ('', None) ):
+                        fileName = filename_raw
+                        if ( itemName == fileName ):
+                            title_raw = item.findtext('title')
+                            if ( title_raw not in ('', None) ):
+                                title = title_raw
+                            version_raw = item.findtext('version')
+                            if ( version_raw not in ('', None) ):
+                                version = version_raw
+                            language_raw = item.findtext('lang')
+                            if ( language_raw not in ('', None) ):
+                                language = language_raw
+                            date_raw = item.findtext('date')
+                            if ( date_raw not in ('', None) ):
+                                date = date_raw
+                            previewPictureURL_raw = item.findtext('previewPictureURL')
+                            if ( previewPictureURL_raw not in ('', None) ):
+                                previewPictureURL = previewPictureURL_raw
+                                
+                                # On verifie si l'image serait deja la
+                                thumbnail, checkPathPic = set_cache_thumb_name( previewPictureURL )
+                                if thumbnail and os.path.isfile( thumbnail ) and hasattr( listitem, 'setThumbnailImage' ):
+                                    #if listitem and hasattr( listitem, 'setThumbnailImage' ):
+                                    listitem.setThumbnailImage( thumbnail )
+                                if os.path.exists(checkPathPic):
+                                    previewPicture = checkPathPic
+                                else:
+                                    # Telechargement et mise a jour de l'image (thread de separe)
+                                    previewPicture = checkPathPic#"downloading"
+                                    self._getImage( previewPictureURL, updateImage_cb=updateImage_cb, listitem=listitem )
+                            previewVideoURL_raw = item.findtext('previewVideoURL')
+                            if ( previewVideoURL_raw not in ('', None) ):
+                                previewVideoURL = previewVideoURL_raw
+                            description_fr_raw = item.findtext('description_fr')
+                            if ( description_fr_raw not in ('', None) ):
+                                description_fr = description_fr_raw
+                            description_en_raw = item.findtext('description_en')
+                            if ( description_en_raw != '' ):
+                                description_en = description_en_raw
+                                
+                            # We exit the loop since we found the file we were looking for
+                            #item.clear()
+                            break
+        
+
+               
+        except Exception, e:
+            #import traceback; traceback.print_exc()
+            logger.EXC_INFO( logger.LOG_ERROR, sys.exc_info(), self )
+        
+        return fileName, title, version, language, date , previewPicture, previewVideoURL, description_fr, description_en, thumbnail
+
+    def _get_settings( self, defaults=False  ):
+        """ reads settings from conf file """
+        self.settings           = Settings().get_settings( defaults=defaults )
+        self.srvHost            = self.mainwin.configManager.getSrvHost()
+        self.srvPassword        = self.mainwin.configManager.getSrvPassword()
+        self.srvUser            = self.mainwin.configManager.getSrvUser()
+        self.srvItemDescripDir  = self.mainwin.configManager.getSrvItemDescripDir()
+        self.srvItemDescripFile = self.mainwin.configManager.getSrvItemDescripFile()
+        
+        logger.LOG( logger.LOG_DEBUG,self.srvHost)
+        logger.LOG( logger.LOG_DEBUG,self.srvItemDescripDir)
+
+    def _downloadFile( self, remoteFilePath, isTBN=True, listitem=None ):
+        """
+        Fonction de telechargement commune : version, archive, script de mise a jour
+        """
+        try:
+            filetodlUrl = remoteFilePath
+            if isTBN:
+                thumbnail, localFilePath = set_cache_thumb_name( remoteFilePath )
+            else:
+                thumbnail = ""
+                localFilePath = os.path.join( self.mainwin.CacheDir, os.path.basename( remoteFilePath ) )
+
+            if not os.path.isfile( localFilePath ):
+                ftp = ftplib.FTP( self.srvHost, self.srvUser, self.srvPassword )
+                localFile = open( localFilePath, "wb" )
+                try:
+                    ftp.retrbinary( 'RETR ' + filetodlUrl, localFile.write )
+                except:
+                    #import traceback; traceback.print_exc()
+                    logger.EXC_INFO( logger.LOG_ERROR, sys.exc_info(), self )
+                localFile.close()
+                ftp.quit()
+
+            # remove file if size is 0 bytes and report error if exists error
+            if isTBN and os.path.isfile( localFilePath ):
+                if os.path.getsize( localFilePath ) <= 0:
+                    os.remove( localFilePath )
+                else:
+                    thumb_size = int( self.thumb_size_on_load )
+                    thumbnail = makeThumbnails( localFilePath, thumbnail, w_h=( thumb_size, thumb_size ) )
+
+            if thumbnail and os.path.isfile( thumbnail ) and hasattr( listitem, 'setThumbnailImage' ):
+                #if listitem and hasattr( listitem, 'setThumbnailImage' ):
+                listitem.setThumbnailImage( thumbnail )
+        except:
+            #import traceback; traceback.print_exc()
+            logger.LOG( logger.LOG_DEBUG, "_downloaddossier: Exception - Impossible de telecharger le fichier: %s", remoteFilePath )
+            logger.EXC_INFO( logger.LOG_ERROR, sys.exc_info(), self )
+
 
 
 class ItemDescription( xbmcgui.WindowXMLDialog ):
@@ -303,7 +512,6 @@ class ItemDescription( xbmcgui.WindowXMLDialog ):
             #logger.LOG( logger.LOG_DEBUG, self.itemType )
             
             self.getControl( 200 ).setVisible( 0 ) # auto busy
-            
             self.fileName, self.title, self.version, self.language, self.date, self.previewPicture, self.previewVideoURL, self.description_fr, self.description_en, thumbnail = self.infoWareHouse.getInfo( itemName=self.itemName, itemType=self.itemType, updateImage_cb=self._updateThumb_cb )
             #logger.LOG( logger.LOG_DEBUG, self.fileName)
             #logger.LOG( logger.LOG_DEBUG, self.title)
@@ -318,6 +526,7 @@ class ItemDescription( xbmcgui.WindowXMLDialog ):
             self._set_controls_visible()
 
         except:
+            #import traceback; traceback.print_exc()
             logger.EXC_INFO( logger.LOG_ERROR, sys.exc_info(), self )
             #self._close_dialog()
         xbmcgui.unlock()
@@ -394,6 +603,7 @@ class ItemDescription( xbmcgui.WindowXMLDialog ):
                 if self.description_en != None:
                     self.getControl( self.CONTROL_DESC_TEXTBOX ).setText( self.description_en )
         except:
+            #import traceback; traceback.print_exc()
             logger.EXC_INFO( logger.LOG_ERROR, sys.exc_info(), self )
 
 
@@ -403,6 +613,7 @@ class ItemDescription( xbmcgui.WindowXMLDialog ):
             self.getControl( self.CONTROL_DOWNLOAD_BUTTON ).setEnabled( False )
             self.getControl( self.CONTROL_DOWNLOAD_BUTTON ).setVisible( False ) 
         except:
+            #import traceback; traceback.print_exc()
             logger.EXC_INFO( logger.LOG_ERROR, sys.exc_info(), self )
         xbmcgui.unlock()
 
@@ -422,6 +633,7 @@ class ItemDescription( xbmcgui.WindowXMLDialog ):
             else:
                 pass
         except:
+            #import traceback; traceback.print_exc()
             logger.EXC_INFO( logger.LOG_ERROR, sys.exc_info(), self )
 
     def onAction( self, action ):
@@ -442,7 +654,8 @@ class ItemInfosManager:
         self.create_info_warehouses()
 
     def create_info_warehouses( self ):
-        self.infoWarehouseFTP = InfoWarehouseXMLFTP( mainwin=self.mainwin )
+        #self.infoWarehouseFTP = InfoWarehouseBSoupXMLFTP( mainwin=self.mainwin )
+        self.infoWarehouseFTP = InfoWarehouseEltTreeXMLFTP( mainwin=self.mainwin )
         #infoWarehouseSQL = InfoWarehouseXMLSQL( mainwin=mainwin )
     
     def show_descript( self, selectedItem , typeItem ):
