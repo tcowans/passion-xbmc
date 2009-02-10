6e556f5d3370341d43e1cd5c3e2226554c2f080e
kottis <kottis@cc7b12b2-8c0a-11dd-abc6-8b02bc135b05>
2009-02-10 10:06:42 +0000

    [Installer Passion-XBMC] - adding backup latest working copy

---
+++ scripts/Installer Passion-XBMC pre1_09022008/Installer Passion-XBMC/resources/libs/GUI/DialogContextMenu.py
@@ -0,0 +1,93 @@
+
+#context menu du plugin "All Game" de frost
+
+#Modules general
+import os
+import sys
+
+#modules XBMC
+import xbmc
+import xbmcgui
+
+#modules custom
+from utilities import *
+
+
+class ContextMenu( xbmcgui.WindowXMLDialog ):
+    # control id's
+    CONTROL_CM_BUTTON_START = 1000
+    CONTROL_CM_BUTTON_END   = 1007
+    CONTROL_CM_BUTTONS = range( CONTROL_CM_BUTTON_START, ( CONTROL_CM_BUTTON_END + 1 ) )
+
+    def __init__( self, *args, **kwargs ):
+        xbmcgui.WindowXMLDialog.__init__( self, *args, **kwargs )
+        self.control_enabled_buttons = range( self.CONTROL_CM_BUTTON_START, ( self.CONTROL_CM_BUTTON_END + 1 ) )
+        self.buttons = kwargs.get( "buttons", {} )
+        xbmc.executebuiltin( "Skin.SetString(totals_cm_buttons,%i)" % ( len( self.buttons ), ) )
+        self.selected = 0
+
+    def onInit( self ):
+        try:
+            xbmcgui.lock()
+            if self.buttons == {}: raise
+            first_cm_button = sorted( self.buttons.keys() )[ 0 ]
+            for key in self.CONTROL_CM_BUTTONS:
+                label = self.buttons.get( key )
+                if label is not None:
+                    if isinstance( label, tuple ):
+                        self.getControl( key ).setLabel( label[ 0 ] )
+                        if label[ 1 ] == "disabled":
+                            self.getControl( key ).setEnabled( 0 )
+                            try: del self.control_enabled_buttons[ self.control_enabled_buttons.index( key ) ]
+                            except: pass
+                            if self.control_enabled_buttons: self.setFocusId( self.control_enabled_buttons[ 0 ] )
+                    else:#if isinstance( label, str ):
+                        self.getControl( key ).setLabel( label )
+                else:
+                    self.getControl( key ).setVisible( 0 )
+            if first_cm_button in self.control_enabled_buttons:
+                self.setFocusId( first_cm_button )
+            xbmcgui.unlock()
+        except:
+            try:logger = sys.modules[ "__main__" ].logger
+            except: import script_log as logger
+            logger.EXC_INFO( logger.LOG_ERROR, sys.exc_info(), self )
+            xbmcgui.unlock()
+            self.close()
+
+    def onFocus( self, controlID ):
+        pass
+
+    def onClick( self, controlID ):
+        if controlID in self.CONTROL_CM_BUTTONS:
+            try:
+                self.selected = controlID
+                try:
+                    # verification du cas de la souris qui peut avoir presser un boutons desactiver
+                    if isinstance( self.buttons.get( self.selected ), tuple ):
+                        if self.buttons.get( self.selected )[ 1 ] == "disabled":
+                            self.selected = 0 #"disabled"
+                except:
+                    pass
+                xbmc.sleep( 10 )
+                self.close()
+            except:
+                pass
+
+    def onAction( self, action ):
+        xbmc.sleep( 10 )
+        if action in ( 9, 10, 117 ):
+            self.selected = 0
+            self.close()
+
+
+def show_context_menu( buttons={} ):
+    file_xml = "passion-ContextMenu.xml"
+    dir_path = os.getcwd().rstrip( ";" )
+    current_skin, force_fallback = getUserSkin()
+
+    w = ContextMenu( file_xml, dir_path, current_skin, force_fallback, buttons=buttons )
+    w.doModal()
+    selected = w.selected
+    del w
+    return selected
---
+++ scripts/Installer Passion-XBMC pre1_09022008/Installer Passion-XBMC/resources/libs/GUI/MainGui.py
@@ -0,0 +1,8 @@
+# futur main gui from INSTALLEUR.py
+
+import xbmcgui
+
+
+class MainWindow( xbmcgui.WindowXML ):
+    pass
+    
