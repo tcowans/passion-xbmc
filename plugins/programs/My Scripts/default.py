1d76e860f22b4efc88391fbf664a07f72f077fef
temhil <temhil@cc7b12b2-8c0a-11dd-abc6-8b02bc135b05>
2009-03-03 00:14:16 +0000

    [My Scripts]: tunk - wrong date

--- plugins/programs/My Scripts/default.py
+++ plugins/programs/My Scripts/default.py
@@ -9,7 +9,7 @@ __url__          = "http://code.google.com/p/passion-xbmc/"
 __svn_url__      = "http://passion-xbmc.googlecode.com/svn/trunk/plugins/programs/"
 __credits__      = "Team XBMC, http://xbmc.org/"
 __platform__     = "xbmc media center"
-__date__         = "02-02-2008"
+__date__         = "02-02-2009"
 __version__      = "1.0"
 __svn_revision__ = "N/A"
 
