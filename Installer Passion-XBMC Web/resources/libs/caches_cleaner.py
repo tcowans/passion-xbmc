813f4d91801b5962aa3b33a9742f91a9a632b7c6
frostbox360 <frostbox360@cc7b12b2-8c0a-11dd-abc6-8b02bc135b05>
2010-01-06 14:52:18 +0000

    [Installer Passion-XBMC Web]: - fixed: caches_cleaner.py TypeError: not all arguments converted during string formatting and variable home_dir  - updated: default.py for append proper date and revision.

--- Installer Passion-XBMC Web/resources/libs/caches_cleaner.py
+++ Installer Passion-XBMC Web/resources/libs/caches_cleaner.py
@@ -1,28 +1,27 @@
 
 # Modules general
 import os
-#import sys
+import sys
 from traceback import print_exc
 
 # Modules XBMC
 import xbmc
 import xbmcgui
 
-# Modules custom
-#if not hasattr( sys.stdout, "terminal" ):
-#    import custom_sys_stdout_stderr as output
 
+#FONCTION POUR RECUPERER LES LABELS DE LA LANGUE.
+_ = sys.modules[ "__main__" ].__language__
 
 DIALOG_PROGRESS = xbmcgui.DialogProgress()
 
 
-par_dir = "../../"
-home_dir = os.sep.join( os.getcwd().split( os.sep )[ :-par_dir.count( "../" ) ] )
+#par_dir = "../../"
+home_dir = os.getcwd()#os.sep.join( os.getcwd().split( os.sep )[ :-par_dir.count( "../" ) ] )
 
 
 def clear( cache_path, excludes=[], t_file=0, t_dir=0 ):
     try:
-        DIALOG_PROGRESS.update( -1, xbmc.getLocalizedString( 32650 ) % cache_path )
+        DIALOG_PROGRESS.update( -1, _( 32650 ) % cache_path )
         for root, dirs, files in os.walk( cache_path, topdown=False ):
             for file in files:
                 fpath = os.path.join( root, file )
@@ -34,10 +33,10 @@ def clear( cache_path, excludes=[], t_file=0, t_dir=0 ):
                         break
                 if not exclude:
                     t_file += 1
-                    DIALOG_PROGRESS.update( -1, xbmc.getLocalizedString( 32651 ) % ( t_file, t_dir, ), xbmc.getLocalizedString( 32652 ) % root, xbmc.getLocalizedString( 32653 ) % file )
+                    DIALOG_PROGRESS.update( -1, _( 32651 ) % ( t_file, t_dir, ), _( 32652 ) % root, _( 32653 ) % file )
                     try:
                         os.remove( fpath )
-                        xbmc.log( "[SCRIPT IPX] Caches Cleaner::clear: '%s'" % fpath, xbmc.LOGDEBUG )
+                        print "[SCRIPT IPX] Caches Cleaner::clear: '%s'" % fpath
                     except:
                         print_exc()
 
@@ -51,10 +50,10 @@ def clear( cache_path, excludes=[], t_file=0, t_dir=0 ):
                         break
                 if not exclude:
                     t_dir += 1
-                    DIALOG_PROGRESS.update( -1, xbmc.getLocalizedString( 32651 ) % ( t_file, t_dir, ), xbmc.getLocalizedString( 32652 ) % root, xbmc.getLocalizedString( 32653 ) % dir )
+                    DIALOG_PROGRESS.update( -1, _( 32651 ) % ( t_file, t_dir, ), _( 32652 ) % root, _( 32653 ) % dir )
                     try:
                         os.rmdir( dpath )
-                        xbmc.log( "[SCRIPT IPX] Caches Cleaner::clear: '%s'" % dpath, xbmc.LOGDEBUG )
+                        print "[SCRIPT IPX] Caches Cleaner::clear: '%s'" % dpath
                     except:
                         print_exc()
     except:
@@ -63,22 +62,17 @@ def clear( cache_path, excludes=[], t_file=0, t_dir=0 ):
 
 
 def Main():
-    cache_list = [
-        xbmc.getLocalizedString( 32654 ),
-        xbmc.getLocalizedString( 32662 ),
-        xbmc.getLocalizedString( 32655 ),
-        xbmc.getLocalizedString( 32656 ) % xbmc.translatePath( "special://temp/" ),
-        xbmc.getLocalizedString( 32656 ) % xbmc.translatePath( "special://profile/cache/" ) ]
+    cache_list = [_ ( 32654 ), _( 32662 ), _( 32655 ),
+        _( 32656 ) % xbmc.translatePath( "special://temp/" ),
+        _( 32656 ) % xbmc.translatePath( "special://profile/cache/" ) ]
     if os.environ.get( "OS", "xbox" ) == "xbox":
-        cache_list += [
-            xbmc.getLocalizedString( 32657 ),
-            xbmc.getLocalizedString( 32658 ) ]
-    selected = xbmcgui.Dialog().select( xbmc.getLocalizedString( 32660 ), cache_list )
+        cache_list += [ _( 32657 ), _( 32658 ) ]
+    selected = xbmcgui.Dialog().select( _( 32660 ), cache_list )
 
     if selected != -1:
         t_file, t_dir = 0, 0
         excludes = [ "weather", "temp/" ]
-        DIALOG_PROGRESS.create( xbmc.getLocalizedString( 32659 ) )
+        DIALOG_PROGRESS.create( _( 32659 ) )
         if selected in [ 0, 1 ]:
             t_file, t_dir = clear( os.path.join( home_dir, "~" ), [], t_file, t_dir )
             t_file, t_dir = clear( os.path.join( home_dir, "cache" ), [], t_file, t_dir )
@@ -100,13 +94,9 @@ def Main():
                 t_file, t_dir = clear( "Y:\\", [], t_file, t_dir )
                 t_file, t_dir = clear( "Z:\\", excludes, t_file, t_dir )
         DIALOG_PROGRESS.close()
-        xbmcgui.Dialog().ok( xbmc.getLocalizedString( 32660 ), cache_list[ selected ], xbmc.getLocalizedString( 32651 ) % ( t_file, t_dir, ) )
+        xbmcgui.Dialog().ok( _( 32660 ), cache_list[ selected ], _( 32651 ) % ( t_file, t_dir, ) )
 
 
 
 if ( __name__ == "__main__" ):
     Main()
-
-    # replace standard stdout and stderr, modified by import custom_sys_stdout_stderr
-    #sys.stdout = sys.stdout.terminal
-    #sys.stderr = sys.stderr.terminal
