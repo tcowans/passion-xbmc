f822fc3af226ae94fb6fa54bb414add780092d87
temhil <temhil@cc7b12b2-8c0a-11dd-abc6-8b02bc135b05>
2010-05-24 04:35:25 +0000

    [Canal Plus]: Release 2.0 :     - Replaced XML parser BeautifulSoup by ElementTree (needed because some XML were not correctly formed)     - Added support of new video stream URL, now http and rtmp urls are both supported

--- plugins/video/Canal Plus/resources/libs/cplusplus.py
+++ plugins/video/Canal Plus/resources/libs/cplusplus.py
@@ -2,9 +2,11 @@ import urllib2, urllib
 import cookielib
 import os,os.path
 import re
-from BeautifulSoup import BeautifulStoneSoup
 import gzip,StringIO
 import socket
+#from xml.dom import minidom #TODO: replace minidom by ET
+import elementtree.ElementTree as ET
+import traceback
 
 # timeout en seconds
 #timeout =  10
@@ -22,60 +24,45 @@ COOKIEFILE = os.path.join(CACHEDIR,'cookies.lwp')
 cj = cookielib.LWPCookieJar()
 
 if os.path.isfile(COOKIEFILE):
-    # si nous avons un fichier cookie déjà sauvegardé
+    # si nous avons un fichier cookie deja sauvegarde
     #  alors charger les cookies dans le Cookie Jar
     cj.load(COOKIEFILE)
          
 xmlParam = "14-1-18/06/2008-0"
-#videoplayerURL = "http://www.canalplus.fr/flash/xml/module/video-player/"
-videoplayerURLOld = "http://www.canalplus.fr/flash/xml/module/video-player/"
+#videoplayerURLOld = "http://www.canalplus.fr/flash/xml/module/video-player/"
 videoplayerURL = "http://webservice.canal-plus.com/rest/bigplayer/"
 #GetMosaicPage = "getMosaic.php"
 GetMosaicPage = "getMEAs/"
 EntrysPage = "video-player-filters.php"
 #ThemesPage = "video-player-thematic.php"
-ThemesPage = "initPlayer"
+ThemesPage = "initPlayer/"
 # TODO : add old base url
 VideoInfoPage = "getVideoInformation.php"
 SearchPage = "search/"
 
     
-def get_page(url,params={},savehtml=True,filename="defaut.html",check_connexion=True,debuglevel=1,nocookie=True):
+def get_page(url,params={},savehtml=True,filename="defaut.html",check_connexion=True,debuglevel=1,nocookie=True,stream=False):
     """
-    télécharge la page avec les paramètres fournis :
-        params : dictionnaire de paramètres
+    telecharge la page avec les parametres fournis :
+        params : dictionnaire de parametres
     Renvoi le code html de la page
     """
 
     #h=urllib2.HTTPHandler(debuglevel=debuglevel)
+    print "get_page:"
+    print url
+    print params
     if nocookie == False:
         h=urllib2.HTTPCookieProcessor(cj)#urllib2.HTTPCookieProcessor(cookiejar)
     request = urllib2.Request(url,urllib.urlencode(params))
-    #request = urllib2.Request(url)
-
-#    request.add_header('Host','www.canalplus.fr')
-#    #request.add_header('Referer', 'http://www.canalplus.fr/flash/loader/loader_canalplus_V0_1.swf')
-#    request.add_header('Referer', 'http://player.canalplus.fr')
-#    request.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; fr; rv:1.9) Gecko/2008052906 Firefox/3.0')
-#    request.add_header('Accept','text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8')
-#    request.add_header('Accept-Language','fr,fr-fr;q=0.8,en-us;q=0.5,en;q=0.3')
-#    request.add_header('Accept-Charset','ISO-8859-1,utf-8;q=0.7,*;q=0.7')
-#    request.add_header('Accept-Encoding','gzip,deflate')
-#    request.add_header('Keep-Alive','300')
-##    for index, cookie in enumerate(cj):
-##        #print index, '  :  ', cookie
-##        request.add_header('Cookie',cookie)
-#    request.add_header('Connection','keep-alive')
-#    request.add_header('Content-type','application/x-www-form-urlencoded')
-#    request.add_header('Content-length',len(urllib.urlencode(params)))
-    #opener = urllib2.build_opener(h)
     opener = urllib2.build_opener()
     urllib2.install_opener(opener)
     connexion = opener.open(request)
     #print connexion.headers
-    #html = connexion.read(int(connexion.headers["Content-Length"]))
+    if stream == True:
+        return connexion # For ET
     html = connexion.read()
-#    open(os.path.join(CACHEDIR,filename).encode('utf8'),"w").write(html)
+    open(os.path.join(CACHEDIR,filename).encode('utf8'),"w").write(html)
 #    if connexion.headers["Content-Encoding"]=="gzip":
 #        compressedstream = StringIO.StringIO(html)
 #        gzipper = gzip.GzipFile(fileobj=compressedstream)
@@ -91,26 +78,11 @@ def get_page(url,params={},savehtml=True,filename="defaut.html",check_connexion=
     return html # data
 
 
-def get_video_infos(videoID):
-    xml = get_page("http://www.canalplus.fr/flash/xml/module/video-player/getVideoInformation.php?video_id="+videoID,
-               filename="video info.xml",debuglevel=1)
-    soup = BeautifulStoneSoup(xml)
-    videoinfos = soup.find("thumbnail")
-
-    return [videoinfos.title.string,
-            videoinfos.summary.string,
-            videoinfos.publication_date.string,
-            videoinfos.image.url.string,
-            videoinfos.smallimage.url.string,
-            videoinfos.theme.string,
-            videoinfos.video.stream_server.string,
-            videoinfos.video.hi.string,
-            videoinfos.video.low.string]
-
 def get_xmlParam():
     """
-    Télécharge la page d'accueil puis cherche et renvoi le xmlParam
+    Telecharge la page d'accueil puis cherche et renvoi le xmlParam
     """
+    print "get_xmlParam called ..."
     html=get_page("http://www.canalplus.fr",filename="index.html",debuglevel=1)
     exp=re.compile(r'"http://www\.canalplus\.fr/flash/xml/configuration/configuration-video-player\.php\?xmlParam=([\-/0-9]*)"')
     try:
@@ -120,157 +92,169 @@ def get_xmlParam():
 
 def get_themes():
     """
-    récupère et retourne une liste des themes et sous thèmes
+    recupere et retourne une liste des themes et sous themes
     [ [ themeid , titre , [ [subtheme_id,titre] , ... ] ] , ... ]
     """
-    #xml = get_page(videoplayerURL + EntrysPage ,filename="entrys.xml",debuglevel=1)
-    #xml = open("entrys.xml").read() #temporaire pour éviter de télécharger
-    #print videoplayerURL + ThemesPage
-    #entrysoup = BeautifulStoneSoup(xml)
-    xml = get_page(videoplayerURL + ThemesPage ,filename="themes.xml",debuglevel=1)
-    #xml = get_page("http://webservice.canal-plus.com/rest/bigplayer/initPlayer",filename="themes.xml",debuglevel=1)
-        
-    #xml = open("themes.xml").read() #temporaire pour éviter de télécharger
-    themesoup = BeautifulStoneSoup(xml)
+    print "get_themes called ..."
+
+    elems = ET.parse(urllib.urlopen(videoplayerURL + ThemesPage)).getroot()
+    #print ET.tostring(elems)
     themes=[]
-##    for theme in entrysoup.findAll("theme"):
-##        themes.append([theme['id'],themesoup.find(id=theme['id']).label.string,get_subthemes(theme['id'])])
-#    for theme in themesoup.findAll("theme"):
-#        themes.append([theme['id'],themesoup.find(id=theme['id']).label.string])
-    #for theme in themesoup.findAll("THEMATIQUE"):
-    for theme in themesoup.findAll("thematique"):
-        #themes.append([theme.ID,theme.NOM.string])
-        if hasattr(theme,'id'):
-            #themes.append([int(theme.id.string),theme.nom.string,get_subthemes(int(theme.id.string))])
-            themes.append([int(theme.id.string),theme.nom.string])
-    print 'themes'
-    print themes
+
+    for theme in elems.find("THEMATIQUES").findall( "THEMATIQUE" ):
+        #print ET.tostring(theme)
+        try:
+            theme_id = int( theme.findtext( "ID" ) )
+            theme_nom = theme.findtext( "NOM" )
+            themes.append( [theme_id, theme_nom] )
+        except:
+            print "get_themes- Erreur durant le parcorus des themes"
+            traceback.print_exc()
+    #print 'themes'
+    #print themes
     return themes
 
 def get_subthemes(theme_id):
     """
-    récupère les sous-themes du thème fourni et renvoi un dictionnaire :
+    recupere les sous-themes du theme fourni et renvoi un dictionnaire :
     { subthemeID : titre , ... }
     """
-    print "get_subthemes"
-    print theme_id
-    #xml = get_page(videoplayerURL + EntrysPage ,filename="entrys.xml",debuglevel=1)
-    xml = get_page(videoplayerURL + ThemesPage ,filename="entrys.xml",debuglevel=1)
-    #xml = open("entrys.xml").read() #temporaire pour éviter de télécharger
-    entrysoup = BeautifulStoneSoup(xml)
-    print entrysoup
+    print "get_subthemes called ..."
+    #print theme_id
+
+    elems = ET.parse(urllib.urlopen(videoplayerURL + ThemesPage)).getroot()
+    #print ET.tostring(elems)
     subthemes=[]
-#    theme = entrysoup.find("theme",{"id":theme_id})
-#    for entry in theme.findAll("entry"):
-#        subtheme.append([entry['id'],entry.string])
-    for theme in entrysoup.findAll("thematique"):
-        if int(theme.id.string) == int(theme_id):
-            for subtheme in theme.findAll("selection"): 
-                if hasattr(subtheme,'id'):
-                    subthemes.append([int(subtheme.id.string),subtheme.nom.string])
-            break
-    print 'subthemes'
-    print subthemes
+
+    for theme in elems.find("THEMATIQUES").findall( "THEMATIQUE" ):
+        #print ET.tostring(theme)
+        try:
+            cur_id = int( theme.findtext( "ID" ) )
+            if int( cur_id ) == int( theme_id ):
+                for subtheme in theme.find("SELECTIONS").findall("SELECTION"): 
+                    subtheme_id = int( subtheme.findtext( "ID" ) )
+                    subtheme_nom = subtheme.findtext( "NOM" )
+                    subthemes.append( [subtheme_id, subtheme_nom] )
+                break
+        except:
+            print "get_subthemes - Erreur durant le parcorus des themes"
+            traceback.print_exc()
+    #print 'subthemes'
+    #print subthemes
     return subthemes
     
 
-#def _searchvideo(keyword):
-#    """
-#    Lance la recherche d'une video et extrait les resultat de la page web recuperee
-#    """
-#    xml = get_page(videoplayerURL + SearchPage + keyword)
-#    soup = BeautifulStoneSoup(xml)
-#    videos=[]
-#    try:
-#        for vid in soup.findAll("video"):
-#            videos.append( { 'videoID' : vid.id.string,
-#                             'title': vid.infos.titrage.titre.string + ': ' + vid.infos.titrage.sous_titre.string,
-#                             'publication_date': vid.infos.titrage.sous_titre.string,
-#                             'image.url': vid.media.images.grand.string,
-#                             'description': vid.infos.description.string,
-#                             'note': float(vid.infos.note.moyenne.string),
-#                             } )
-#    except Exception, e:
-#        error=1
-#        print "cplusplus: Something went wrong get_videos!"
-#        print e
-#        print (str(sys.exc_info()[0]))
-#        traceback.print_exc()
-#    print "videos"
-#    print videos
-#    return videos
-
-def get_videos(xmlParam,theme_id,subtheme_id,keyword=''):
+def get_videos( subtheme_id,keyword='' ):
     """
-    retourne l'équivalent de la mosaique canalplus sous forme d'une liste de dictionnaires
+    retourne l'equivalent de la mosaique canalplus sous forme d'une liste de dictionnaires
     [ { videoid , title , publication , imageURL , theme , visualized , note , duration } ]
     """
-#    xml = get_page(videoplayerURL + GetMosaicPage+"?xmlParam="+xmlParam,params={"theme_id":theme_id,
-#                                                                                "content_id":"",
-#                                                                                "subtheme_id":subtheme_id,
-#                                                                                "keywords":keyword},
-#                   filename="listingmozaic.xml",debuglevel=1)
+    print "get_videos called ..."
     videos=[]
     firstTag = ""
     if ( keyword != '' ):
-#        print 'Search'
+        print 'Searching: %s'%keyword
 #        print videoplayerURL + SearchPage + keyword
         #videos = _searchvideo( keyword )
-        xml = get_page(videoplayerURL + SearchPage + keyword)
-        firstTag = "video"
+        urlxml = videoplayerURL + SearchPage + keyword
+        firstTag = "VIDEO"
     else:
-        xml = get_page(videoplayerURL + GetMosaicPage + subtheme_id)
-        firstTag = "mea"
+        urlxml = videoplayerURL + GetMosaicPage + subtheme_id
+        firstTag = "MEA"
         
-    #xml = open("listingmozaic.xml").read()
-    soup = BeautifulStoneSoup(xml)
-#    for thumb in soup.findAll("thumbnail"):
-#        videos.append( { 'videoID' : thumb['id'],
-#                         'title': thumb.title.string,
-#                         'publication_date': thumb.publication_date.string,
-#                         'image.url': thumb.image.url.string,
-#                         'theme': thumb.theme.string,
-#                         'visualized': thumb.visualized.String,
-#                         'note': thumb.note.string,
-#                         'duration': thumb.duration.string
-#                         } )
-    try:
-        for vid in soup.findAll(firstTag):
-            videos.append( { 'videoID' : vid.id.string,
-                             'title': vid.infos.titrage.titre.string + ': ' + vid.infos.titrage.sous_titre.string,
-                             'publication_date': vid.infos.titrage.sous_titre.string,
-                             'image.url': vid.media.images.grand.string,
-                             'description': vid.infos.description.string,
-                             'note': float(vid.infos.note.moyenne.string),
+
+    elems = ET.parse( urllib.urlopen( urlxml ) ).getroot()
+    #print ET.tostring(elems)
+
+    for vid in elems.findall( firstTag ):
+        #print ET.tostring( vid )
+        try:
+            videoID         = int( vid.findtext( "ID" ) )
+            #title           = vid.find( "INFOS" ).find( "TITRAGE" ).findtext( "TITRE" ) + ': ' + vid.find( "INFOS" ).find( "TITRAGE" ).findtext( "SOUS_TITRE" )
+            title           = vid.find( "INFOS" ).find( "TITRAGE" ).findtext( "TITRE" ) + ': ' #+ vid.find( "INFOS" ).find( "TITRAGE" ).findtext( "SOUS_TITRE" )
+            publicationDate = vid.find( "INFOS" ).find( "TITRAGE" ).findtext( "SOUS_TITRE" )
+            description     = vid.find( "INFOS" ).findtext( "DESCRIPTION" )
+            note            = float( vid.find( "INFOS" ).find( "NOTE" ).findtext( "MOYENNE" ) )
+            imageURL        = vid.find( "MEDIA" ).find( "IMAGES" ).findtext( "GRAND" )
+            
+            videos.append( { 'videoID' : videoID,
+                             'title': title,
+                             'publication_date': publicationDate,
+                             'image.url': imageURL,
+                             'description': description,
+                             'note': note,
                              } )
-    except Exception, e:
-        error=1
-        print "cplusplus: Something went wrong get_videos!"
-        print e
-        print (str(sys.exc_info()[0]))
-        traceback.print_exc()
-    print "videos"
-    print videos
+        except:
+            print "cplusplus: error while retrieving videos list and info"
+            traceback.print_exc()
+    #print "videos"
+    #print videos
     return videos
 
-def get_info(videoID):
+
+def get_info( videoID ):
     """
-    retrouve les informations de l'ID de vidéo fourni
+    retrouve les informations de l'ID de video fourni
     """
-    #xml = get_page(videoplayerURL +"getVideoInformation.php?video_id=%s"%videoID,filename="video info %s.xml"%videoID,debuglevel=1)
-    xml = get_page(videoplayerURLOld +"getVideoInformation.php?video_id=%s"%videoID,filename="video info %s.xml"%videoID,debuglevel=1)
-    soup = BeautifulStoneSoup(xml)
-    videoinfos = soup.find("thumbnail")
-    return {'title' : videoinfos.title.string,
-            'summary' : videoinfos.summary.string,
-            'publication_date' : videoinfos.publication_date.string,
-            'image.url' : videoinfos.image.url.string.decode(),
-            'smallimage.url' : videoinfos.smallimage.url.string.decode(),
-            'theme' : videoinfos.theme.string,
-            'video.stream_server' : videoinfos.video.stream_server.string,
-            'video.hi' : videoinfos.video.hi.string,
-            'video.low' : videoinfos.video.low.string
-            }
+    print "get_videos called for video ID: %d"%videoID
+    
+    #print "get_info - Retrieving page:" + videoplayerURL +"getVideosLiees/%d"%videoID
+
+    elems = ET.parse(urllib.urlopen(videoplayerURL +"getVideosLiees/%d"%videoID)).getroot()
+
+    type = 'rtmp'
+
+    #print elems
+    #print elems.tag
+    #print ET.tostring(elems)
+    #print elems.find( "VIDEO" )
+    for videoinfos in elems.findall( "VIDEO" ):
+        #print 'videoinfos'
+        #print videoinfos
+        
+        videoinfos_id = int ( videoinfos.findtext( "ID" ) )
+        videoinfos_type = videoinfos.findtext( "TYPE" )
+        videoinfos_title = videoinfos.findtext( "INFOS/TITRAGE/TITRE" )#.encode("cp1252")
+        videoinfos_description = videoinfos.findtext( "INFOS/DESCRIPTION" )#.encode("cp1252")
+        videoinfos_videoHD = videoinfos.findtext( "MEDIA/VIDEOS/HAUT_DEBIT" )
+        videoinfos_videoBD = videoinfos.findtext( "MEDIA/VIDEOS/BAS_DEBIT" )
+        videoinfos_publication_date = videoinfos.findtext( "INFOS/PUBLICATION/DATE" )
+        videoinfos_image = videoinfos.findtext( "MEDIA/IMAGES/GRAND" )
+        videoinfos_smallimage = videoinfos.findtext( "MEDIA/IMAGES/PETIT" )
+        videoinfos_categorie = videoinfos.findtext( "RUBRIQUAGE/CATEGORIE" )
+        #print "videoinfos_id"
+        #print videoinfos_id
+        #print videoinfos_type
+        #print videoinfos_description
+        #print videoinfos_videoHD
+        #print videoinfos_videoBD
+        if videoinfos_id == videoID:
+            # Found
+            print "Video ID found"
+            if videoinfos_type == "VOD PROG":
+                type = 'http'
+                videoHD_URL = videoinfos_videoHD
+                videoBD_URL = videoinfos_videoBD
+            else:
+                type = 'rtmp'
+                videoHD_URL = videoinfos_videoHD.replace("rtmp://vod-fms.canalplus.fr","rtmp://vod-fms.canalplus.fr:1935").replace(".flv","")
+                videoBD_URL = videoinfos_videoBD.replace("rtmp://vod-fms.canalplus.fr","rtmp://vod-fms.canalplus.fr:1935").replace(".flv","")
+            
+            #print "videoHD_URL = %s"%videoHD_URL
+            #print "videoBD_URL = %s"%videoBD_URL
+            return {'title' : videoinfos_title,
+                    'summary' : videoinfos_description,
+                    'publication_date' :  videoinfos_publication_date,
+                    'image.url' : videoinfos_image,
+                    'smallimage.url' : videoinfos_smallimage,
+                    'theme' : videoinfos_categorie,
+                    'video.stream_server' : "",
+                    'video.hi' : videoHD_URL,
+                    'video.low' : videoBD_URL
+                    }
+    print "get_info - video info not FOUND - ERROR"
+    return None
+
 def Cache_Pic(url,filename):
     try:
         if not os.path.exists(filename):
@@ -283,7 +267,7 @@ def Cache_Pic(url,filename):
     
 def DL_video(url,fichier,ProgressUpdate=None,ProgressObject=None,debuglevel=0):
     """
-    télécharge la video à l'url mentionnée
+    telecharge la video a l'url mentionnee
     """
     if os.path.exists(fichier):
         for i in range(100):
@@ -327,7 +311,7 @@ def DL_video(url,fichier,ProgressUpdate=None,ProgressObject=None,debuglevel=0):
     return 1
 
 if __name__ == "__main__":
-    ###avant tout, récupérons le xmlParam
+    ###avant tout, recuperons le xmlParam
     xmlParam = get_xmlParam()
     #xmlParam = "14-1-24/06/2008-0"
     print get_themes()
