2ccb2a7fb0573f733842f93c7ac4ca77852e34be
frostbox360 <frostbox360@cc7b12b2-8c0a-11dd-abc6-8b02bc135b05>
2010-05-25 14:50:16 +0000

    [Addons - Scripts] - sync with XBMC SVN Addons

--- addons/script.game.arkanoid/default.py
+++ addons/script.game.arkanoid/default.py
@@ -1,13 +1,14 @@
 
 # Script Constants
 __script__        = "Arkanoid"
+__scriptID__      = "script.game.arkanoid"
 __author__        = "Frost"
 __url__           = "http://passion-xbmc.org/"
 __svn_url__       = "http://passion-xbmc.googlecode.com/svn/trunk/addons/script.game.arkanoid/"
 __credits__       = "Team XBMC, http://xbmc.org/"
 __platform__      = "xbmc media center, [ALL]"
 __started__       = "26-02-2010"
-__date__          = "29-04-2010"
+__date__          = "26-05-2010"
 __version__       = "pre-1.0.0"
 __statut__        = "Beta1"
 __svn_revision__  = "$Revision$".replace( "Revision", "" ).strip( "$: " )
@@ -30,7 +31,7 @@ except:
     import traceback; traceback.print_exc()
 
 def settings():
-    if OK: SETTINGS = xbmc.Settings( __script__ )
+    if OK: SETTINGS = xbmc.Settings( __scriptID__ )
     else: SETTINGS = xbmc.Settings( os.getcwd() )
     return SETTINGS
 
