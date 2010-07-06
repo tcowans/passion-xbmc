e59ec5c2b3c7d1e8b69709134228f0e5a5de8b84
ppic.ppic <ppic.ppic@cc7b12b2-8c0a-11dd-abc6-8b02bc135b05>
2010-07-06 22:09:55 +0000

    [script.tv.show.next.aired] initial commit

---
+++ addons/script.tv.show.next.aired/default.py
@@ -0,0 +1,29 @@
+
+__script__       = "TV-Show Next-Aired"
+__author__       = "Ppic, Frost"
+__url__          = "http://code.google.com/p/passion-xbmc/"
+__svn_url__      = ""
+__credits__      = "Team Passion-XBMC, http://passion-xbmc.org/"
+__platform__     = "xbmc media center, [ALL, without XBox]"
+__date__         = "23-06-2010"
+__version__      = "1.0"
+__svn_revision__  = "$Revision$"
+
+
+import os
+import sys
+from traceback import print_exc
+
+
+RESOURCES_PATH = os.path.join( os.getcwd() , "resources" )
+sys.path.append( os.path.join( RESOURCES_PATH, "lib" ) )
+
+
+try:
+    from scraper import getDetails
+    import next_aired_dialog
+    next_aired_list = getDetails()
+    next_aired_dialog.MyDialog(next_aired_list)
+    print "### Exiting ... "
+except:
+    print_exc()
