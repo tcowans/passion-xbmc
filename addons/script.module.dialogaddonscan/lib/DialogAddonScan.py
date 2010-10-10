4b3cbedc46b549351539578a4b20a04ce9aa38f6
frostbox360 <frostbox360@cc7b12b2-8c0a-11dd-abc6-8b02bc135b05>
2010-10-10 18:17:59 +0000

    [script.module.dialogaddonscan] - added: iscanceled()    - function to cancel scan:    -- ( xbmcgui.Window( xbmcgui.getCurrentWindowId() ).setProperty( "CancelDialogAddonScan", "true" ) )  - added: small settings for displaying dialog positions...  - changed: properties name ( DialogAddonScanIsAlive -> DialogAddonScan.IsAlive )  - added: skin xml ( but is not displaying, is loaded for keeping layout of controls )
     Note: The "readme.txt" file is not updated. See Demo() function in lib/DialogAddonScan.py

--- addons/script.module.dialogaddonscan/lib/DialogAddonScan.py
+++ addons/script.module.dialogaddonscan/lib/DialogAddonScan.py
@@ -1,172 +1,32 @@
 
 import os
 import sys
-import time
 from traceback import print_exc
 
 import xbmc
-import xbmcgui
 from xbmcaddon import Addon
+
+from gui import Window
+
 
 __settings__  = Addon( "script.module.dialogaddonscan" )
 __addonName__ = __settings__.getAddonInfo( "name" )
-__addonDir__  = __settings__.getAddonInfo( "path" )
-
-XBMC_SKIN  = xbmc.getSkinDir()
-SKINS_PATH = os.path.join( __addonDir__, "resources", "skins" )
-ADDON_SKIN = ( "default", XBMC_SKIN )[ os.path.exists( os.path.join( SKINS_PATH, XBMC_SKIN ) ) ]
-MEDIA_PATH = os.path.join( SKINS_PATH, ADDON_SKIN, "media" )
-
-# WINDOW DEFAULT COORDINATES
-WINDOW_POSX = 720
-WINDOW_POSY = 0
-
-# FONTS
-FONT_HEADING = "font10_title", '0xFFEB9E17'
-FONT_LINE    = "font10", '0xFFFFFFFF'
-
-# TEXTURES
-TEXTURE_BG               = os.path.join( MEDIA_PATH, "BackgroundPanel.png" )
-PROGRESS_TEXTURE_BG      = os.path.join( MEDIA_PATH, "ProgressBack.png" )
-PROGRESS_TEXTURE_LEFT    = os.path.join( MEDIA_PATH, "ProgressLeft.png" )
-PROGRESS_TEXTURE_MID     = os.path.join( MEDIA_PATH, "ProgressFront.png" )
-PROGRESS_TEXTURE_RIGHT   = os.path.join( MEDIA_PATH, "ProgressRight.png" )
-PROGRESS_TEXTURE_OVERLAY = os.path.join( MEDIA_PATH, "ProgressOverlay.png" )
-
-# VALIDE TEXTURES
-TEXTURE_BG               = ( "", TEXTURE_BG )[ os.path.exists( TEXTURE_BG ) ]
-PROGRESS_TEXTURE_BG      = ( "", PROGRESS_TEXTURE_BG )[ os.path.exists( PROGRESS_TEXTURE_BG ) ]
-PROGRESS_TEXTURE_LEFT    = ( "", PROGRESS_TEXTURE_LEFT )[ os.path.exists( PROGRESS_TEXTURE_LEFT ) ]
-PROGRESS_TEXTURE_MID     = ( "", PROGRESS_TEXTURE_MID )[ os.path.exists( PROGRESS_TEXTURE_MID ) ]
-PROGRESS_TEXTURE_RIGHT   = ( "", PROGRESS_TEXTURE_RIGHT )[ os.path.exists( PROGRESS_TEXTURE_RIGHT ) ]
-PROGRESS_TEXTURE_OVERLAY = ( "", PROGRESS_TEXTURE_OVERLAY )[ os.path.exists( PROGRESS_TEXTURE_OVERLAY ) ]
-
-
-class xbmcguiWindowError( WindowsError ):
-    def __init__( self, winError=None ):
-        WindowsError.__init__( self, winError )
-
-
-class Window:
-    def __init__( self, parent_win=None, **kwargs ):
-        if xbmc.getInfoLabel( "Window.Property(DialogAddonScanIsAlive)" ) == "true":
-            raise xbmcguiWindowError( "DialogAddonScanIsAlive: Not possible to over scan!" )
-
-        # http://wiki.xbmc.org/index.php?title=Window_IDs
-        self.window = parent_win
-
-        self.background = None
-        self.heading = None
-        self.label = None
-        self.progress1 = None
-        self.progress2 = None
-
-    def setupWindow( self ):
-        error = 0
-        try: xbmcgui.lock()
-        except: pass
-        #get the id for the current 'active' window as an integer.
-        try: current_window = xbmcgui.getCurrentWindowId()
-        except: current_window = self.window
-
-        #if self.window is None and hasattr( current_window, "__int__" ):
-        #    self.window = xbmcgui.Window( current_window )
-        if hasattr( current_window, "__int__" ) and current_window != self.window:
-            self.removeControls()
-            self.window = xbmcgui.Window( current_window )
-            self.initialize()
-
-        #print hasattr( self.window, "addControl" )
-        if not self.window or not hasattr( self.window, "addControl" ):
-            self.removeControls()
-            error = 1
-
-        xbmcgui.unlock()
-        if error:
-            raise xbmcguiWindowError( "xbmcgui.Window(%s)" % repr( current_window ) )
-
-        self.window.setProperty( "DialogAddonScanIsAlive", "true" )
-        #if xbmc.getInfoLabel( "Window.Property(DialogAddonScanIsAlive)" ) == "true":
-        #    print self.window.getProperty( "DialogAddonScanIsAlive" )
-        #    print xbmc.getInfoLabel( "Window.Property(DialogAddonScanIsAlive)" )
-
-    def initialize( self ):
-        try:
-            # http://passion-xbmc.org/gros_fichiers/XBMC%20Python%20Doc/xbmc_svn/xbmcgui.html#ControlImage
-            # BACKGROUND
-            self.background = xbmcgui.ControlImage( WINDOW_POSX, WINDOW_POSY, 400, 70, TEXTURE_BG )
-            self.window.addControl( self.background )
-
-            # http://passion-xbmc.org/gros_fichiers/XBMC%20Python%20Doc/xbmc_svn/xbmcgui.html#ControlLabel
-            # HEADING
-            self.heading = xbmcgui.ControlLabel( WINDOW_POSX+15, WINDOW_POSY+4, 370, 18,
-                self.header,
-                FONT_HEADING[ 0 ],
-                FONT_HEADING[ 1 ]
-                )
-            self.window.addControl( self.heading )
-
-            # LABEL
-            self.label = xbmcgui.ControlLabel( WINDOW_POSX+15, WINDOW_POSY+20, 370, 18,
-                self.line,
-                FONT_LINE[ 0 ],
-                FONT_LINE[ 1 ]
-                )
-            self.window.addControl( self.label )
-
-            # http://passion-xbmc.org/gros_fichiers/XBMC%20Python%20Doc/xbmc_svn/xbmcgui.html#ControlProgress
-            # CURRENT PROGRESS
-            self.progress1 = xbmcgui.ControlProgress( WINDOW_POSX+15, WINDOW_POSY+38, 370, 8,
-                texturebg=PROGRESS_TEXTURE_BG,
-                textureleft=PROGRESS_TEXTURE_LEFT,
-                texturemid=PROGRESS_TEXTURE_MID,
-                textureright=PROGRESS_TEXTURE_RIGHT,
-                textureoverlay=PROGRESS_TEXTURE_OVERLAY
-                )
-            self.window.addControl( self.progress1 )
-
-            # PROGRESS OF LISTING
-            self.progress2 = xbmcgui.ControlProgress( WINDOW_POSX+15, WINDOW_POSY+45, 370, 8,
-                texturebg=PROGRESS_TEXTURE_BG,
-                textureleft=PROGRESS_TEXTURE_LEFT,
-                texturemid=PROGRESS_TEXTURE_MID,
-                textureright=PROGRESS_TEXTURE_RIGHT,
-                textureoverlay=PROGRESS_TEXTURE_OVERLAY
-                )
-            self.window.addControl( self.progress2 )
-        except:
-            print_exc()
-
-    def removeControls( self ):
-        if self.progress2:
-            try: self.window.removeControl( self.progress2 )
-            except: pass
-        if self.progress1:
-            try: self.window.removeControl( self.progress1 )
-            except: pass
-        if self.label:
-            try: self.window.removeControl( self.label )
-            except: pass
-        if self.heading:
-            try: self.window.removeControl( self.heading )
-            except: pass
-        if self.background:
-            try: self.window.removeControl( self.background )
-            except: pass
-        try: self.window.clearProperty( "DialogAddonScanIsAlive" )
-        except: pass
 
 
 class AddonScan( Window ):
     def __init__( self, parent_win=None, **kwargs ):
         # get class Window object
         Window.__init__( self, parent_win, **kwargs )
-
+        self.canceled = False
         self.header = ""
         self.line = ""
 
     def close( self ):
+        self.canceled = True
+        xbmc.sleep( 100 )
         self.removeControls()
+        del self.controls
+        del self.window
 
     def create( self, line1="", line2="" ):
         self.header = line1 or __addonName__
@@ -174,60 +34,84 @@ class AddonScan( Window ):
         self.update( 0, 0, line1, line2 )
 
     def iscanceled( self ):
-        pass
+        """ @ mudole.py
+            if xbmc.getInfoLabel( "Window.Property(DialogAddonScanIsAlive)" ) == "true":
+                # ok rajoute un bouton stop dans le context menu
+                c_items += [ ( "Stop Addon Scan", "RunPlugin(%s?action=stopscan)" % sys.argv[ 0 ] ) ]
+                listitem.addContextMenuItems( c_items )
+
+            @ main.py
+            if "stopscan" in sys.argv[ 2 ]:
+                window = xbmcgui.Window( xbmcgui.getCurrentWindowId() )
+                window.setProperty( "CancelDialogAddonScan", "true" )
+        """
+        return self.canceled
 
     def update( self, percent1=0, percent2=0, line1="", line2="" ):
         self.setupWindow()
-        if line1:
+        if line1 and hasattr( self.heading, "setLabel" ):
             # set heading
             try: self.heading.setLabel( line1 )
             except: print_exc()
-        if line2:
+        if line2 and hasattr( self.label, "setLabel" ):
             # set label
             self.line = line2
             try: self.label.setLabel( line2 )
             except: print_exc()
-        if percent1:
+        if percent1 and hasattr( self.progress1, "setPercent" ):
             # set current progress
             try: self.progress1.setPercent( percent1 )
             except: print_exc()
-        if percent2: 
+        if percent2 and hasattr( self.progress2, "setPercent" ):
             # set progress of listing
             try: self.progress2.setPercent( percent2 )
             except: print_exc()
 
 
+
 def Demo():
-    try:
-        scan = AddonScan()
-        # create dialog
-        scan.create( "Demo: "+__addonName__ )
+    import xbmcgui
+    selected = xbmcgui.Dialog().select( "Demo: "+__addonName__, [ "Show demo scan", "Open settings" ] )
+    if selected == 0:
+        from time import sleep
+        from gui import xbmcguiWindowError
+        try:
+            scan = AddonScan()
+            # create dialog
+            scan.create( "Demo: "+__addonName__ )
 
-        for pct in range( 101 ):
-            percent2 = pct
-            percent1 = percent2*10
-            while percent1 > 100:
-                percent1 -= 100
-            line2 = "Progress1 [B]%i%%[/B]   |   Progress2 [B]%i%%[/B]" % ( percent1, percent2 )
+            for pct in range( 101 ):
+                percent2 = pct
+                percent1 = percent2*10
+                while percent1 > 100:
+                    percent1 -= 100
+                line2 = "Progress1 [B]%i%%[/B]   |   Progress2 [B]%i%%[/B]" % ( percent1, percent2 )
 
-            # update dialog ( [ int1, int2, line1=str, line2=str ] ) all args optional
-            scan.update( percent1, percent2, line2=line2 )
-            time.sleep( .25 )
+                # update dialog ( [ percent1=int, percent2=int, line1=str, line2=str ] ) all args is optional
+                scan.update( percent1, percent2, line2=line2 )
 
-        # close dialog and auto destroy all controls
-        scan.close()
-    except xbmcguiWindowError:
-        print_exc()
-    except:
-        print_exc()
+                # if is canceled stop 
+                if scan.iscanceled():
+                    break
+
+                sleep( .1 )
+
+            # close dialog and auto destroy all controls
+            scan.close()
+        except xbmcguiWindowError:
+            print_exc()
+        except:
+            print_exc()
+
+    elif selected == 1:
+        __settings__.openSettings()
 
 
 
 if ( __name__ == "__main__" ):
     try:
-        #args = "dit moi quoi faire :)"
-        print sys.argv[ 1 ]
+        # settings called from other addon
+        if sys.argv[ 1 ].lower() == "opensettins":
+            __settings__.openSettings()
     except:
-        print "ok rien ...."
-
-    Demo()
+        print_exc()
--- addons/script.module.dialogaddonscan/readme.txt
+++ addons/script.module.dialogaddonscan/readme.txt
@@ -1,27 +1,27 @@
 
-METHOD USE
+METHODE USE
 
 1. Add required import in your addon.xml
-  - example 1: ( recommended )
+  - exemple 1: ( recommended )
     <requires>
         ...
         <import addon="script.module.dialogaddonscan" version="1.0.0"/>
     </requires>
 
-  - example 2:
+  - exemple 2:
     import os, sys
     sys.path.append( os.path.join( "MY ADDON PATH", "script.module.dialogaddonscan", "lib" ) )
 
 2. test addon scan demo
     import DialogAddonScan
-    #test simple example on module DialogAddonScan
+    #test simple exemple on module DialogAddonScan
     DialogAddonScan.Demo()
 
 3. Normal use addon scan
     import DialogAddonScan
     scan = DialogAddonScan.AddonScan()
     # create dialog
-    scan.create( "Title" )
+    scan.create( "Tilte" )
 
     for pct in range( 101 ):
         percent2 = pct
@@ -38,9 +38,9 @@ METHOD USE
     scan.close()
 
 4. Scan on background :)
-  - Create lib "test_AddonScanInBackground.py" copy example "3. Normal use addon scan" or "2. test addon scan demo"
+  - Create lib "test_AddonScanInBackground.py" copy exemple "3. Normal use addon scan" or "2. test addon scan demo"
 
-  - In main code or in function, add executebuiltin::RunScript
+  - In main code or in fonction, add executebuiltin::RunScript
     import os, xbmc
     script = os.path.join( os.getcwd(), "test_AddonScanInBackground.py" )
     xbmc.executebuiltin( "RunScript(%s)" % ( script ) )
@@ -48,22 +48,7 @@ METHOD USE
    -And now test background :)
 
 5. if your want set dialog position
-    import DialogAddonScan
-    DialogAddonScan.WINDOW_POSX = 720
-    DialogAddonScan.WINDOW_POSY = 0
-    scan = DialogAddonScan.AddonScan()
-    >>>
+    ...
 
-6. All Data for custom modify
-    from DialogAddonScan import *
-    # FONTS
-    FONT_HEADING = "font10_title", '0xFFEB9E17'
-    FONT_LINE    = "font10", '0xFFFFFFFF'
-
-    # TEXTURES
-    TEXTURE_BG               = "BackgroundPanel.png"
-    PROGRESS_TEXTURE_BG      = "ProgressBack.png"
-    PROGRESS_TEXTURE_LEFT    = "ProgressLeft.png"
-    PROGRESS_TEXTURE_MID     = "ProgressFront.png"
-    PROGRESS_TEXTURE_RIGHT   = "ProgressRight.png"
-    PROGRESS_TEXTURE_OVERLAY = "ProgressOverlay.png"
+6. All Data for custom modif
+    ...
