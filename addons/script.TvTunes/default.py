f539b8ff33041055eed8608ecd87f95ccc72da17
ppic.ppic <ppic.ppic@cc7b12b2-8c0a-11dd-abc6-8b02bc135b05>
2010-10-26 21:50:28 +0000

    [script.TvTunes]v0.6.0: - change volume setting - rewrite code - changed thread control - corrections on volume control, no only used when theme is playing - control if current playing is a theme for stop condtion - do not interrupt music playing if user is listen to music. - fixed theme not playinf when re enter the same show - fixed theme downloader was not playing full theme

--- addons/script.TvTunes/default.py
+++ addons/script.TvTunes/default.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 import sys
 import os
-
+import xbmcaddon
+__settings__ = xbmcaddon.Addon( "script.TvTunes" )
 RESOURCES_PATH = os.path.join( os.getcwd() , "resources" )
 sys.path.append( RESOURCES_PATH )
 try:
@@ -12,13 +13,11 @@ except:
     # no params passed
     params = {}   
 if params.get("backend", False ): 
-    import xbmcaddon
-    __settings__ = xbmcaddon.Addon( "script.TvTunes" )
+
     loop = __settings__.getSetting("loop")
-    downvolume = __settings__.getSetting("downvolume")+"0"
-    if xbmc.getInfoLabel( "Window(10025).Property(TvTunesIsAlive)" ) != "true":xbmc.executebuiltin('XBMC.RunScript(%s,loop=%s&downvolume=%s)' % (os.path.join( RESOURCES_PATH , "tvtunes_backend.py"), loop , downvolume ))
+    downvolume = __settings__.getSetting("downvolume")
+    if xbmc.getInfoLabel( "Window(10025).Property(TvTunesIsRunning)" ) != "true":
+        xbmc.executebuiltin('XBMC.RunScript(%s,loop=%s&downvolume=%s)' % (os.path.join( RESOURCES_PATH , "tvtunes_backend.py"), loop , int( float( downvolume ) ) ))
 else: 
-    import xbmcaddon
-    __settings__ = xbmcaddon.Addon( "script.TvTunes" )     
     print " %s v%s" % ( __settings__.getAddonInfo("id") , __settings__.getAddonInfo("version") )
     import tvtunes_scraper
