4868854f6be71e97c27990e35104561af0978a58
FrostBox360 <FrostBox360@cc7b12b2-8c0a-11dd-abc6-8b02bc135b05>
2012-03-02 23:37:24 +0000

    [weather.google] 2.0.0  - added: a lot of new properties. see README.txt  - added: Moon and Earth Phases images (auto refresh every 1 hour). see properties in README.txt  - changed: property "Current.Locale.Sunrise" for "Current.Sunrise"  - changed: property "Current.Locale.Sunset" for "Current.Sunset"  - removed: extra settings  - removed: dharma/xbox compatibility  - cleanup...

--- trunk/addons/eden-pre/weather.google/default.py
+++ trunk/addons/eden-pre/weather.google/default.py
@@ -3,39 +3,41 @@
     url: http://passion-xbmc.googlecode.com/svn/trunk/addons/eden-pre/weather.google/
 """
 
-from resources.lib.weather import *
+import os
+import sys
 
-#print sys.argv
-
-loc_index = "".join( sys.argv[ 1: ] )
-if loc_index.isdigit():
-    GWS = getWeatherSettings2
-    SP  = SetProperties2
-else:
-    loc_index = None
-    GWS = getWeatherSettings
-    SP  = SetProperties
+import xbmc
 
 
-def Main( retry=3 ):
-    try:
-        ClearProperties()
-        SetProperty( "WeatherProvider", "Google Weather" )
-
-        city, LocationIndex = GWS( loc_index )
-
-        if not city: raise ( city, LocationIndex )
-
-        weather = get_weather( city, LANG )
-        if weather:
-            SP( weather, LocationIndex=LocationIndex )
-    except:
-        print_exc()
-        retry -= 1
-        if retry > 0:
-            Main( retry )
-
+def compatibility():
+    from xbmcaddon import Addon
+    Addon = Addon( "weather.google" )
+    # get old settings and if exists delete data xml 
+    if Addon.getSetting( 'risingsun_city' ) or Addon.getSetting( 'risingsun1_city' ):
+        icon = Addon.getAddonInfo( "icon" )
+        xml  = Addon.getAddonInfo( 'profile' ) + 'settings.xml'
+        del Addon
+        import xbmcvfs
+        xbmcvfs.delete( xml )
+        if not xbmcvfs.exists( xml ):
+            #notifie user
+            xbmc.executebuiltin( "XBMC.Notification(Weather Google,Your settings have been renewed! Sorry!,10000,%s)" % icon )
 
 
 if __name__ == "__main__":
-    Main()
+    compatibility()
+    loc_index = "".join( sys.argv[ 1: ] )
+    if loc_index.isdigit():
+        xbmc.executebuiltin( "RunScript(%s)" % os.path.join( sys.path[ 0 ], "resources", "lib", "moon_and_earth_phases.py" ) )
+        from resources.lib.weather import Main
+        Main( loc_index )
+
+    elif loc_index.lower() == "refreshextra":
+        #refresh/load extra info (run script timeanddate)
+        xbmc.executebuiltin( "CancelAlarm(moon_earth_phases,true)" )
+        xbmc.executebuiltin( "RunScript(%s)" % os.path.join( sys.path[ 0 ], "resources", "lib", "timeanddate.py" ) )
+        xbmc.executebuiltin( "RunScript(%s)" % os.path.join( sys.path[ 0 ], "resources", "lib", "moon_and_earth_phases.py" ) )
+
+    else:
+        # run old script (weather_dharma)
+        loc_index = None
